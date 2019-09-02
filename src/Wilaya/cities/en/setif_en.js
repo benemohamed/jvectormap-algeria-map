204945f180ebbcd29418ad0a59743ecf8dc12a39
Mohamed Ben <mohamed@ipfinder.io>
2019-09-02 22:52:32 +0100

    Add Communes de la wilaya setif

---
+++ src/Wilaya/cities/en/setif_en.js
@@ -0,0 +1,256 @@
+jQuery.fn.vectorMap("addMap", "setif_en", {
+  insets: [
+    {
+      width: 1100,
+      top: 0,
+      height: 1100,
+      bbox: [{ y: 0, x: 300 }, { y: 0, x: 0 }],
+      left: 0
+    }
+  ],
+  paths: {
+    "1": {
+      path: "",
+      name: "Setif"
+    },
+    "2": {
+      path: "",
+      name: "Ain El Kebira"
+    },
+    "3": {
+      path: "",
+      name: "Beni Aziz"
+    },
+    "4": {
+      path: "",
+      name: "Ouled Sidi Ahmed"
+    },
+    "5": {
+      path: "",
+      name: "Boutaleb"
+    },
+    "6": {
+      path: "",
+      name: "Ain Roua"
+    },
+    "7": {
+      path: "",
+      name: "Draa Kebila"
+    },
+    "8": {
+      path: "",
+      name: "Bir El Arch"
+    },
+    "9": {
+      path: "",
+      name: "Beni Chebana"
+    },
+    "10": {
+      path: "",
+      name: "Ouled Tebben"
+    },
+    "11": {
+      path: "",
+      name: "Hamma"
+    },
+    "12": {
+      path: "",
+      name: "Maaouia"
+    },
+    "13": {
+      path: "",
+      name: "Ain Legraj"
+    },
+    "14": {
+      path: "",
+      name: "Ain Abessa"
+    },
+    "15": {
+      path: "",
+      name: "Dehamcha"
+    },
+    "16": {
+      path: "",
+      name: "Babor"
+    },
+    "17": {
+      path: "",
+      name: "Guidjel"
+    },
+    "18": {
+      path: "",
+      name: "Ain Lahdjar"
+    },
+    "19": {
+      path: "",
+      name: "Bousselam"
+    },
+    "20": {
+      path: "",
+      name: "El Eulma"
+    },
+    "21": {
+      path: "",
+      name: "Djemila"
+    },
+    "22": {
+      path: "",
+      name: "Beni Ouartilane"
+    },
+    "23": {
+      path: "",
+      name: "Rosfa"
+    },
+    "24": {
+      path: "",
+      name: "Ouled Addouane"
+    },
+    "25": {
+      path: "",
+      name: "Belaa"
+    },
+    "26": {
+      path: "",
+      name: "Ain Arnat"
+    },
+    "27": {
+      path: "",
+      name: "Amoucha"
+    },
+    "28": {
+      path: "",
+      name: "Ain Oulmane"
+    },
+    "29": {
+      path: "",
+      name: "Beidha Bordj"
+    },
+    "30": {
+      path: "",
+      name: "Bouandas"
+    },
+    "31": {
+      path: "",
+      name: "Bazer Sakhra"
+    },
+    "32": {
+      path: "",
+      name: "Hammam Essokhna"
+    },
+    "33": {
+      path: "",
+      name: "Mezloug"
+    },
+    "34": {
+      path: "",
+      name: "Bir Haddada"
+    },
+    "35": {
+      path: "",
+      name: "Serdj El Ghoul"
+    },
+    "36": {
+      path: "",
+      name: "Harbil"
+    },
+    "37": {
+      path: "",
+      name: "El Ouricia"
+    },
+    "38": {
+      path: "",
+      name: "Tizi Nbechar"
+    },
+    "39": {
+      path: "",
+      name: "Salah Bey"
+    },
+    "40": {
+      path: "",
+      name: "Ain Azal"
+    },
+    "41": {
+      path: "",
+      name: "Guenzet"
+    },
+    "42": {
+      path: "",
+      name: "Talaifacene"
+    },
+    "43": {
+      path: "",
+      name: "Bougaa"
+    },
+    "44": {
+      path: "",
+      name: "Beni Fouda"
+    },
+    "45": {
+      path: "",
+      name: "Tachouda"
+    },
+    "46": {
+      path: "",
+      name: "Beni Mouhli"
+    },
+    "47": {
+      path: "",
+      name: "Ouled Sabor"
+    },
+    "48": {
+      path: "",
+      name: "Guellal"
+    },
+    "49": {
+      path: "",
+      name: "Ain Sebt"
+    },
+    "50": {
+      path: "",
+      name: "Hammam Guergour"
+    },
+    "51": {
+      path: "",
+      name: "Ait Naoual Mezada"
+    },
+    "52": {
+      path: "",
+      name: "Ksar El Abtal"
+    },
+    "53": {
+      path: "",
+      name: "Beni Hocine"
+    },
+    "54": {
+      path: "",
+      name: "Ait Tizi"
+    },
+    "55": {
+      path: "",
+      name: "Maouklane"
+    },
+    "56": {
+      path: "",
+      name: "Guelta Zerka"
+    },
+    "57": {
+      path: "",
+      name: "Oued El Barad"
+    },
+    "58": {
+      path: "",
+      name: "Taya"
+    },
+    "59": {
+      path: "",
+      name: "El Ouldja"
+    },
+    "60": {
+      path: "",
+      name: "Tella"
+    }
+  },
+  height: 1100,
+  projection: { type: "mill", centralMeridian: 0.0 },
+  width: 1100
+});
