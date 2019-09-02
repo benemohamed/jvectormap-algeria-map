9d6cf8fcd3ac1cba7183206be86b76f4a7c4c30f
Mohamed Ben <mohamed@ipfinder.io>
2019-09-02 22:52:31 +0100

    Add Communes de la wilaya bejaia

---
+++ src/Wilaya/cities/en/bejaia_en.js
@@ -0,0 +1,224 @@
+jQuery.fn.vectorMap("addMap", "bejaia_en", {
+  insets: [
+    {
+      width: 1100,
+      top: 0,
+      height: 1100,
+      bbox: [{ y: 0, x: 300 }, { y: 0, x: 0 }],
+      left: 0
+    }
+  ],
+  paths: {
+    "1": {
+      path: "",
+      name: "Bejaia"
+    },
+    "2": {
+      path: "",
+      name: "Amizour"
+    },
+    "3": {
+      path: "",
+      name: "Ferraoun"
+    },
+    "4": {
+      path: "",
+      name: "Taourirt Ighil"
+    },
+    "5": {
+      path: "",
+      name: "Chelata"
+    },
+    "6": {
+      path: "",
+      name: "Tamokra"
+    },
+    "7": {
+      path: "",
+      name: "Timzrit"
+    },
+    "8": {
+      path: "",
+      name: "Souk El Thenine"
+    },
+    "9": {
+      path: "",
+      name: "Mcisna"
+    },
+    "10": {
+      path: "",
+      name: "Thinabdher"
+    },
+    "11": {
+      path: "",
+      name: "Tichi"
+    },
+    "12": {
+      path: "",
+      name: "Semaoun"
+    },
+    "13": {
+      path: "",
+      name: "Kendira"
+    },
+    "14": {
+      path: "",
+      name: "Tifra"
+    },
+    "15": {
+      path: "",
+      name: "Ighram"
+    },
+    "16": {
+      path: "",
+      name: "Amalou"
+    },
+    "17": {
+      path: "",
+      name: "Ighil Ali"
+    },
+    "18": {
+      path: "",
+      name: "Ifelain Ilmathen"
+    },
+    "19": {
+      path: "",
+      name: "Toudja"
+    },
+    "20": {
+      path: "",
+      name: "Darguina"
+    },
+    "21": {
+      path: "",
+      name: "Sidi Ayad"
+    },
+    "22": {
+      path: "",
+      name: "Aokas"
+    },
+    "23": {
+      path: "",
+      name: "Beni Djellil"
+    },
+    "24": {
+      path: "",
+      name: "Adekar"
+    },
+    "25": {
+      path: "",
+      name: "Akbou"
+    },
+    "26": {
+      path: "",
+      name: "Seddouk"
+    },
+    "27": {
+      path: "",
+      name: "Tazmalt"
+    },
+    "28": {
+      path: "",
+      name: "Ait Rizine"
+    },
+    "29": {
+      path: "",
+      name: "Chemini"
+    },
+    "30": {
+      path: "",
+      name: "Souk Oufella"
+    },
+    "31": {
+      path: "",
+      name: "Taskriout"
+    },
+    "32": {
+      path: "",
+      name: "Tibane"
+    },
+    "33": {
+      path: "",
+      name: "Tala Hamza"
+    },
+    "34": {
+      path: "",
+      name: "Barbacha"
+    },
+    "35": {
+      path: "",
+      name: "Beni Ksila"
+    },
+    "36": {
+      path: "",
+      name: "Ouzallaguen"
+    },
+    "37": {
+      path: "",
+      name: "Bouhamza"
+    },
+    "38": {
+      path: "",
+      name: "Beni Melikeche"
+    },
+    "39": {
+      path: "",
+      name: "Sidi Aich"
+    },
+    "40": {
+      path: "",
+      name: "El Kseur"
+    },
+    "41": {
+      path: "",
+      name: "Melbou"
+    },
+    "42": {
+      path: "",
+      name: "Akfadou"
+    },
+    "43": {
+      path: "",
+      name: "Leflaye"
+    },
+    "44": {
+      path: "",
+      name: "Kherrata"
+    },
+    "45": {
+      path: "",
+      name: "Draa Kaid"
+    },
+    "46": {
+      path: "",
+      name: "Tamridjet"
+    },
+    "47": {
+      path: "",
+      name: "Ait Smail"
+    },
+    "48": {
+      path: "",
+      name: "Boukhelifa"
+    },
+    "49": {
+      path: "",
+      name: "Tizi Nberber"
+    },
+    "50": {
+      path: "",
+      name: "Beni Maouch"
+    },
+    "51": {
+      path: "",
+      name: "Oued Ghir"
+    },
+    "52": {
+      path: "",
+      name: "Boudjellil"
+    }
+  },
+  height: 1100,
+  projection: { type: "mill", centralMeridian: 0.0 },
+  width: 1100
+});
