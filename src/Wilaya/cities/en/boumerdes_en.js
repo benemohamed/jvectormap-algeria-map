2ffa2255669f09f4c0d76b3d45eb17610ef2274f
Mohamed Ben <mohamed@ipfinder.io>
2019-09-02 22:52:33 +0100

    Add Communes de la wilaya boumerdes

--- src/Wilaya/cities/en/boumerdes_en.js
+++ src/Wilaya/cities/en/boumerdes_en.js
@@ -1,9 +1,9 @@
 jQuery.fn.vectorMap("addMap", "boumerdes_en", {
   insets: [
     {
-      width: 500,
+      width: 1100,
       top: 0,
-      height: 420,
+      height: 1100,
       bbox: [{ y: 0, x: 300 }, { y: 0, x: 0 }],
       left: 0
     }
@@ -11,10 +11,134 @@ jQuery.fn.vectorMap("addMap", "boumerdes_en", {
   paths: {
     "1": {
       path: "",
-      name: ""
+      name: "Boumerdes"
+    },
+    "2": {
+      path: "",
+      name: "Boudouaou"
+    },
+    "3": {
+      path: "",
+      name: "Afir"
+    },
+    "4": {
+      path: "",
+      name: "Bordj Menaiel"
+    },
+    "5": {
+      path: "",
+      name: "Baghlia"
+    },
+    "6": {
+      path: "",
+      name: "Sidi Daoud"
+    },
+    "7": {
+      path: "",
+      name: "Naciria"
+    },
+    "8": {
+      path: "",
+      name: "Djinet"
+    },
+    "9": {
+      path: "",
+      name: "Isser"
+    },
+    "10": {
+      path: "",
+      name: "Zemmouri"
+    },
+    "11": {
+      path: "",
+      name: "Si Mustapha"
+    },
+    "12": {
+      path: "",
+      name: "Tidjelabine"
+    },
+    "13": {
+      path: "",
+      name: "Chabet El Ameur"
+    },
+    "14": {
+      path: "",
+      name: "Thenia"
+    },
+    "15": {
+      path: "",
+      name: "Timezrit"
+    },
+    "16": {
+      path: "",
+      name: "Corso"
+    },
+    "17": {
+      path: "",
+      name: "Ouled Moussa"
+    },
+    "18": {
+      path: "",
+      name: "Larbatache"
+    },
+    "19": {
+      path: "",
+      name: "Bouzegza Keddara"
+    },
+    "20": {
+      path: "",
+      name: "Taourga"
+    },
+    "21": {
+      path: "",
+      name: "Ouled Aissa"
+    },
+    "22": {
+      path: "",
+      name: "Ben Choud"
+    },
+    "23": {
+      path: "",
+      name: "Dellys"
+    },
+    "24": {
+      path: "",
+      name: "Ammal"
+    },
+    "25": {
+      path: "",
+      name: "Beni Amrane"
+    },
+    "26": {
+      path: "",
+      name: "Souk El Had"
+    },
+    "27": {
+      path: "",
+      name: "Boudouaou El Bahri"
+    },
+    "28": {
+      path: "",
+      name: "Ouled Hedadj"
+    },
+    "29": {
+      path: "",
+      name: "Laghata"
+    },
+    "30": {
+      path: "",
+      name: "Hammedi"
+    },
+    "31": {
+      path: "",
+      name: "Khemis El Khechna"
+    },
+    "32": {
+      path: "",
+      name: "El Kharrouba"
     }
   },
-  height: 420,
+  height: 1100,
   projection: { type: "mill", centralMeridian: 0.0 },
-  width: 500
+  width: 1100
 });
