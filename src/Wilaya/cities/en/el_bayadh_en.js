50c534df12660d7331de7ace8d0a0c2cc4a61bfa
Mohamed Ben <mohamed@ipfinder.io>
2019-09-02 22:52:33 +0100

    Add Communes de la wilaya el_bayadh

--- src/Wilaya/cities/en/el_bayadh_en.js
+++ src/Wilaya/cities/en/el_bayadh_en.js
@@ -1,9 +1,9 @@
 jQuery.fn.vectorMap("addMap", "el_bayadh_en", {
   insets: [
     {
-      width: 500,
+      width: 1100,
       top: 0,
-      height: 420,
+      height: 1100,
       bbox: [{ y: 0, x: 300 }, { y: 0, x: 0 }],
       left: 0
     }
@@ -11,10 +11,94 @@ jQuery.fn.vectorMap("addMap", "el_bayadh_en", {
   paths: {
     "1": {
       path: "",
-      name: ""
+      name: "El Bayadh"
+    },
+    "2": {
+      path: "",
+      name: "Rogassa"
+    },
+    "3": {
+      path: "",
+      name: "Stitten"
+    },
+    "4": {
+      path: "",
+      name: "Brezina"
+    },
+    "5": {
+      path: "",
+      name: "Ghassoul"
+    },
+    "6": {
+      path: "",
+      name: "Boualem"
+    },
+    "7": {
+      path: "",
+      name: "El Abiodh Sidi Cheikh"
+    },
+    "8": {
+      path: "",
+      name: "Ain El Orak"
+    },
+    "9": {
+      path: "",
+      name: "Arbaouat"
+    },
+    "10": {
+      path: "",
+      name: "Bougtoub"
+    },
+    "11": {
+      path: "",
+      name: "El Kheither"
+    },
+    "12": {
+      path: "",
+      name: "Kef El Ahmar"
+    },
+    "13": {
+      path: "",
+      name: "Boussemghoun"
+    },
+    "14": {
+      path: "",
+      name: "Chellala"
+    },
+    "15": {
+      path: "",
+      name: "Krakda"
+    },
+    "16": {
+      path: "",
+      name: "El Bnoud"
+    },
+    "17": {
+      path: "",
+      name: "Cheguig"
+    },
+    "18": {
+      path: "",
+      name: "Sidi Ameur"
+    },
+    "19": {
+      path: "",
+      name: "El Mehara"
+    },
+    "20": {
+      path: "",
+      name: "Tousmouline"
+    },
+    "21": {
+      path: "",
+      name: "Sidi Slimane"
+    },
+    "22": {
+      path: "",
+      name: "Sidi Tifour"
     }
   },
-  height: 420,
+  height: 1100,
   projection: { type: "mill", centralMeridian: 0.0 },
-  width: 500
+  width: 1100
 });
