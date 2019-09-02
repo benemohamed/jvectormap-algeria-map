12632d39ad568379b0778aad42c4782fa5f8c015
Mohamed Ben <mohamed@ipfinder.io>
2019-09-02 22:52:31 +0100

    Add Communes de la wilaya laghouat

--- src/Wilaya/cities/en/laghouat_en.js
+++ src/Wilaya/cities/en/laghouat_en.js
@@ -1,9 +1,9 @@
 jQuery.fn.vectorMap("addMap", "laghouat_en", {
   insets: [
     {
-      width: 500,
+      width: 1100,
       top: 0,
-      height: 420,
+      height: 1100,
       bbox: [{ y: 0, x: 300 }, { y: 0, x: 0 }],
       left: 0
     }
@@ -11,10 +11,102 @@ jQuery.fn.vectorMap("addMap", "laghouat_en", {
   paths: {
     "1": {
       path: "",
-      name: ""
+      name: "Laghouat"
+    },
+    "2": {
+      path: "",
+      name: "Ksar El Hirane"
+    },
+    "3": {
+      path: "",
+      name: "Benacer Ben Chohra"
+    },
+    "4": {
+      path: "",
+      name: "Sidi Makhlouf"
+    },
+    "5": {
+      path: "",
+      name: "Hassi Delaa"
+    },
+    "6": {
+      path: "",
+      name: "Hassi R Mel"
+    },
+    "7": {
+      path: "",
+      name: "Ain Mahdi"
+    },
+    "8": {
+      path: "",
+      name: "Tadjmout"
+    },
+    "9": {
+      path: "",
+      name: "Kheneg"
+    },
+    "10": {
+      path: "",
+      name: "Gueltat Sidi Saad"
+    },
+    "11": {
+      path: "",
+      name: "Ain Sidi Ali"
+    },
+    "12": {
+      path: "",
+      name: "Beidha"
+    },
+    "13": {
+      path: "",
+      name: "Brida"
+    },
+    "14": {
+      path: "",
+      name: "El Ghicha"
+    },
+    "15": {
+      path: "",
+      name: "Hadj Mechri"
+    },
+    "16": {
+      path: "",
+      name: "Sebgag"
+    },
+    "17": {
+      path: "",
+      name: "Taouiala"
+    },
+    "18": {
+      path: "",
+      name: "Tadjrouna"
+    },
+    "19": {
+      path: "",
+      name: "Aflou"
+    },
+    "20": {
+      path: "",
+      name: "El Assafia"
+    },
+    "21": {
+      path: "",
+      name: "Oued Morra"
+    },
+    "22": {
+      path: "",
+      name: "Oued M Zi"
+    },
+    "23": {
+      path: "",
+      name: "El Haouaita"
+    },
+    "24": {
+      path: "",
+      name: "Sidi Bouzid"
     }
   },
-  height: 420,
+  height: 1100,
   projection: { type: "mill", centralMeridian: 0.0 },
-  width: 500
+  width: 1100
 });
