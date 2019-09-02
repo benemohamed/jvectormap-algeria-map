982d565b2fbb61d6d87973dbb05eb4450012b4be
Mohamed Ben <mohamed@ipfinder.io>
2019-09-02 22:52:32 +0100

    Add Communes de la wilaya djelfa

--- src/Wilaya/cities/en/djelfa_en.js
+++ src/Wilaya/cities/en/djelfa_en.js
@@ -1,9 +1,9 @@
 jQuery.fn.vectorMap("addMap", "djelfa_en", {
   insets: [
     {
-      width: 500,
+      width: 1100,
       top: 0,
-      height: 420,
+      height: 1100,
       bbox: [{ y: 0, x: 300 }, { y: 0, x: 0 }],
       left: 0
     }
@@ -11,10 +11,150 @@ jQuery.fn.vectorMap("addMap", "djelfa_en", {
   paths: {
     "1": {
       path: "",
-      name: ""
+      name: "Djelfa"
+    },
+    "2": {
+      path: "",
+      name: "Moudjebara"
+    },
+    "3": {
+      path: "",
+      name: "El Guedid"
+    },
+    "4": {
+      path: "",
+      name: "Hassi Bahbah"
+    },
+    "5": {
+      path: "",
+      name: "Ain Maabed"
+    },
+    "6": {
+      path: "",
+      name: "Sed Rahal"
+    },
+    "7": {
+      path: "",
+      name: "Feidh El Botma"
+    },
+    "8": {
+      path: "",
+      name: "Birine"
+    },
+    "9": {
+      path: "",
+      name: "Bouira Lahdeb"
+    },
+    "10": {
+      path: "",
+      name: "Zaccar"
+    },
+    "11": {
+      path: "",
+      name: "El Khemis"
+    },
+    "12": {
+      path: "",
+      name: "Sidi Baizid"
+    },
+    "13": {
+      path: "",
+      name: "Mliliha"
+    },
+    "14": {
+      path: "",
+      name: "El Idrissia"
+    },
+    "15": {
+      path: "",
+      name: "Douis"
+    },
+    "16": {
+      path: "",
+      name: "Hassi El Euch"
+    },
+    "17": {
+      path: "",
+      name: "Messaad"
+    },
+    "18": {
+      path: "",
+      name: "Guettara"
+    },
+    "19": {
+      path: "",
+      name: "Sidi Ladjel"
+    },
+    "20": {
+      path: "",
+      name: "Had Sahary"
+    },
+    "21": {
+      path: "",
+      name: "Guernini"
+    },
+    "22": {
+      path: "",
+      name: "Selmana"
+    },
+    "23": {
+      path: "",
+      name: "Ain Chouhada"
+    },
+    "24": {
+      path: "",
+      name: "Oum Laadham"
+    },
+    "25": {
+      path: "",
+      name: "Dar Chouikh"
+    },
+    "26": {
+      path: "",
+      name: "Charef"
+    },
+    "27": {
+      path: "",
+      name: "Beni Yacoub"
+    },
+    "28": {
+      path: "",
+      name: "Zaafrane"
+    },
+    "29": {
+      path: "",
+      name: "Deldoul"
+    },
+    "30": {
+      path: "",
+      name: "Ain El Ibel"
+    },
+    "31": {
+      path: "",
+      name: "Ain Oussera"
+    },
+    "32": {
+      path: "",
+      name: "Benhar"
+    },
+    "33": {
+      path: "",
+      name: "Hassi Fedoul"
+    },
+    "34": {
+      path: "",
+      name: "Amourah"
+    },
+    "35": {
+      path: "",
+      name: "Ain Fekka"
+    },
+    "36": {
+      path: "",
+      name: "Tadmit"
     }
   },
-  height: 420,
+  height: 1100,
   projection: { type: "mill", centralMeridian: 0.0 },
-  width: 500
+  width: 1100
 });
