c0af790adaf34321ed5800bcfffb460926460dd7
Mohamed Ben <mohamed@ipfinder.io>
2019-09-02 22:52:33 +0100

    Add Communes de la wilaya tissemsilt

--- src/Wilaya/cities/en/tissemsilt_en.js
+++ src/Wilaya/cities/en/tissemsilt_en.js
@@ -1,9 +1,9 @@
 jQuery.fn.vectorMap("addMap", "tissemsilt_en", {
   insets: [
     {
-      width: 500,
+      width: 1100,
       top: 0,
-      height: 420,
+      height: 1100,
       bbox: [{ y: 0, x: 300 }, { y: 0, x: 0 }],
       left: 0
     }
@@ -11,10 +11,94 @@ jQuery.fn.vectorMap("addMap", "tissemsilt_en", {
   paths: {
     "1": {
       path: "",
-      name: ""
+      name: "Tissemsilt"
+    },
+    "2": {
+      path: "",
+      name: "Bordj Bou Naama"
+    },
+    "3": {
+      path: "",
+      name: "Theniet El Had"
+    },
+    "4": {
+      path: "",
+      name: "Lazharia"
+    },
+    "5": {
+      path: "",
+      name: "Beni Chaib"
+    },
+    "6": {
+      path: "",
+      name: "Lardjem"
+    },
+    "7": {
+      path: "",
+      name: "Melaab"
+    },
+    "8": {
+      path: "",
+      name: "Sidi Lantri"
+    },
+    "9": {
+      path: "",
+      name: "Bordj El Emir Abdelkader"
+    },
+    "10": {
+      path: "",
+      name: "Layoune"
+    },
+    "11": {
+      path: "",
+      name: "Khemisti"
+    },
+    "12": {
+      path: "",
+      name: "Ouled Bessem"
+    },
+    "13": {
+      path: "",
+      name: "Ammari"
+    },
+    "14": {
+      path: "",
+      name: "Youssoufia"
+    },
+    "15": {
+      path: "",
+      name: "Sidi Boutouchent"
+    },
+    "16": {
+      path: "",
+      name: "Larbaa"
+    },
+    "17": {
+      path: "",
+      name: "Maasem"
+    },
+    "18": {
+      path: "",
+      name: "Sidi Abed"
+    },
+    "19": {
+      path: "",
+      name: "Tamalaht"
+    },
+    "20": {
+      path: "",
+      name: "Sidi Slimane"
+    },
+    "21": {
+      path: "",
+      name: "Boucaid"
+    },
+    "22": {
+      path: "",
+      name: "Beni Lahcene"
     }
   },
-  height: 420,
+  height: 1100,
   projection: { type: "mill", centralMeridian: 0.0 },
-  width: 500
+  width: 1100
 });
