8295cd416d0a56d7c7b94ee8004e6916fc423332
Mohamed Ben <mohamed@ipfinder.io>
2019-09-02 22:52:32 +0100

    Add Communes de la wilaya sidi_bel_abbes

--- src/Wilaya/cities/en/sidi_bel_abbes_en.js
+++ src/Wilaya/cities/en/sidi_bel_abbes_en.js
@@ -1,9 +1,9 @@
 jQuery.fn.vectorMap("addMap", "sidi_bel_abbes_en", {
   insets: [
     {
-      width: 500,
+      width: 1100,
       top: 0,
-      height: 420,
+      height: 1100,
       bbox: [{ y: 0, x: 300 }, { y: 0, x: 0 }],
       left: 0
     }
@@ -11,10 +11,214 @@ jQuery.fn.vectorMap("addMap", "sidi_bel_abbes_en", {
   paths: {
     "1": {
       path: "",
-      name: ""
+      name: "Sidi Bel Abbes"
+    },
+    "2": {
+      path: "",
+      name: "Tessala"
+    },
+    "3": {
+      path: "",
+      name: "Sidi Brahim"
+    },
+    "4": {
+      path: "",
+      name: "Mostefa Ben Brahim"
+    },
+    "5": {
+      path: "",
+      name: "Telagh"
+    },
+    "6": {
+      path: "",
+      name: "Mezaourou"
+    },
+    "7": {
+      path: "",
+      name: "Boukhanafis"
+    },
+    "8": {
+      path: "",
+      name: "Sidi Ali Boussidi"
+    },
+    "9": {
+      path: "",
+      name: "Badredine El Mokrani"
+    },
+    "10": {
+      path: "",
+      name: "Marhoum"
+    },
+    "11": {
+      path: "",
+      name: "Tafissour"
+    },
+    "12": {
+      path: "",
+      name: "Amarnas"
+    },
+    "13": {
+      path: "",
+      name: "Tilmouni"
+    },
+    "14": {
+      path: "",
+      name: "Sidi Lahcene"
+    },
+    "15": {
+      path: "",
+      name: "Ain Thrid"
+    },
+    "16": {
+      path: "",
+      name: "Makedra"
+    },
+    "17": {
+      path: "",
+      name: "Tenira"
+    },
+    "18": {
+      path: "",
+      name: "Moulay Slissen"
+    },
+    "19": {
+      path: "",
+      name: "El Hacaiba"
+    },
+    "20": {
+      path: "",
+      name: "Hassi Zehana"
+    },
+    "21": {
+      path: "",
+      name: "Tabia"
+    },
+    "22": {
+      path: "",
+      name: "Merine"
+    },
+    "23": {
+      path: "",
+      name: "Ras El Ma"
+    },
+    "24": {
+      path: "",
+      name: "Ain Tindamine"
+    },
+    "25": {
+      path: "",
+      name: "Ain Kada"
+    },
+    "26": {
+      path: "",
+      name: "Mcid"
+    },
+    "27": {
+      path: "",
+      name: "Sidi Khaled"
+    },
+    "28": {
+      path: "",
+      name: "Ain El Berd"
+    },
+    "29": {
+      path: "",
+      name: "Sfissef"
+    },
+    "30": {
+      path: "",
+      name: "Ain Adden"
+    },
+    "31": {
+      path: "",
+      name: "Oued Taourira"
+    },
+    "32": {
+      path: "",
+      name: "Dhaya"
+    },
+    "33": {
+      path: "",
+      name: "Zerouala"
+    },
+    "34": {
+      path: "",
+      name: "Lamtar"
+    },
+    "35": {
+      path: "",
+      name: "Sidi Chaib"
+    },
+    "36": {
+      path: "",
+      name: "Sidi Dahou Dezairs"
+    },
+    "37": {
+      path: "",
+      name: "Oued Sbaa"
+    },
+    "38": {
+      path: "",
+      name: "Boudjebaa El Bordj"
+    },
+    "39": {
+      path: "",
+      name: "Sehala Thaoura"
+    },
+    "40": {
+      path: "",
+      name: "Sidi Yacoub"
+    },
+    "41": {
+      path: "",
+      name: "Sidi Hamadouche"
+    },
+    "42": {
+      path: "",
+      name: "Belarbi"
+    },
+    "43": {
+      path: "",
+      name: "Oued Sefioun"
+    },
+    "44": {
+      path: "",
+      name: "Teghalimet"
+    },
+    "45": {
+      path: "",
+      name: "Ben Badis"
+    },
+    "46": {
+      path: "",
+      name: "Sidi Ali Benyoub"
+    },
+    "47": {
+      path: "",
+      name: "Chetouane Belaila"
+    },
+    "48": {
+      path: "",
+      name: "Bir El Hammam"
+    },
+    "49": {
+      path: "",
+      name: "Taoudmout"
+    },
+    "50": {
+      path: "",
+      name: "Redjem Demouche"
+    },
+    "51": {
+      path: "",
+      name: "Benachiba Chelia"
+    },
+    "52": {
+      path: "",
+      name: "Hassi Dahou"
     }
   },
-  height: 420,
+  height: 1100,
   projection: { type: "mill", centralMeridian: 0.0 },
-  width: 500
+  width: 1100
 });
