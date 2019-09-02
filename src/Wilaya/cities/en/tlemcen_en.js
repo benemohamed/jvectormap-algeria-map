1cdf937eef03ed7f5c8218155ad467d0b9d04a7a
Mohamed Ben <mohamed@ipfinder.io>
2019-09-02 22:52:32 +0100

    Add Communes de la wilaya tlemcen

--- src/Wilaya/cities/en/tlemcen_en.js
+++ src/Wilaya/cities/en/tlemcen_en.js
@@ -1,9 +1,9 @@
 jQuery.fn.vectorMap("addMap", "tlemcen_en", {
   insets: [
     {
-      width: 500,
+      width: 1100,
       top: 0,
-      height: 420,
+      height: 1100,
       bbox: [{ y: 0, x: 300 }, { y: 0, x: 0 }],
       left: 0
     }
@@ -11,10 +11,218 @@ jQuery.fn.vectorMap("addMap", "tlemcen_en", {
   paths: {
     "1": {
       path: "",
-      name: ""
+      name: "Tlemcen"
+    },
+    "2": {
+      path: "",
+      name: "Beni Mester"
+    },
+    "3": {
+      path: "",
+      name: "Ain Tallout"
+    },
+    "4": {
+      path: "",
+      name: "Remchi"
+    },
+    "5": {
+      path: "",
+      name: "El Fehoul"
+    },
+    "6": {
+      path: "",
+      name: "Sabra"
+    },
+    "7": {
+      path: "",
+      name: "Ghazaouet"
+    },
+    "8": {
+      path: "",
+      name: "Souani"
+    },
+    "9": {
+      path: "",
+      name: "Djebala"
+    },
+    "10": {
+      path: "",
+      name: "El Gor"
+    },
+    "11": {
+      path: "",
+      name: "Oued Chouly"
+    },
+    "12": {
+      path: "",
+      name: "Ain Fezza"
+    },
+    "13": {
+      path: "",
+      name: "Ouled Mimoun"
+    },
+    "14": {
+      path: "",
+      name: "Amieur"
+    },
+    "15": {
+      path: "",
+      name: "Ain Youcef"
+    },
+    "16": {
+      path: "",
+      name: "Zenata"
+    },
+    "17": {
+      path: "",
+      name: "Beni Snous"
+    },
+    "18": {
+      path: "",
+      name: "Bab El Assa"
+    },
+    "19": {
+      path: "",
+      name: "Dar Yaghmouracene"
+    },
+    "20": {
+      path: "",
+      name: "Fellaoucene"
+    },
+    "21": {
+      path: "",
+      name: "Azails"
+    },
+    "22": {
+      path: "",
+      name: "Sebbaa Chioukh"
+    },
+    "23": {
+      path: "",
+      name: "Terni Beni Hediel"
+    },
+    "24": {
+      path: "",
+      name: "Bensekrane"
+    },
+    "25": {
+      path: "",
+      name: "Ain Nehala"
+    },
+    "26": {
+      path: "",
+      name: "Hennaya"
+    },
+    "27": {
+      path: "",
+      name: "Maghnia"
+    },
+    "28": {
+      path: "",
+      name: "Hammam Boughrara"
+    },
+    "29": {
+      path: "",
+      name: "Souahlia"
+    },
+    "30": {
+      path: "",
+      name: "Msirda Fouaga"
+    },
+    "31": {
+      path: "",
+      name: "Ain Fetah"
+    },
+    "32": {
+      path: "",
+      name: "El Aricha"
+    },
+    "33": {
+      path: "",
+      name: "Souk Thlata"
+    },
+    "34": {
+      path: "",
+      name: "Sidi Abdelli"
+    },
+    "35": {
+      path: "",
+      name: "Sebdou"
+    },
+    "36": {
+      path: "",
+      name: "Beni Ouarsous"
+    },
+    "37": {
+      path: "",
+      name: "Sidi Medjahed"
+    },
+    "38": {
+      path: "",
+      name: "Beni Boussaid"
+    },
+    "39": {
+      path: "",
+      name: "Marsa Ben Mhidi"
+    },
+    "40": {
+      path: "",
+      name: "Nedroma"
+    },
+    "41": {
+      path: "",
+      name: "Sidi Djillali"
+    },
+    "42": {
+      path: "",
+      name: "Beni Bahdel"
+    },
+    "43": {
+      path: "",
+      name: "El Bouihi"
+    },
+    "44": {
+      path: "",
+      name: "Honaine"
+    },
+    "45": {
+      path: "",
+      name: "Tianet"
+    },
+    "46": {
+      path: "",
+      name: "Ouled Riyah"
+    },
+    "47": {
+      path: "",
+      name: "Bouhlou"
+    },
+    "48": {
+      path: "",
+      name: "Souk El Khemis"
+    },
+    "49": {
+      path: "",
+      name: "Ain Ghoraba"
+    },
+    "50": {
+      path: "",
+      name: "Chetouane"
+    },
+    "51": {
+      path: "",
+      name: "Mansourah"
+    },
+    "52": {
+      path: "",
+      name: "Beni Semiel"
+    },
+    "53": {
+      path: "",
+      name: "Ain Kebira"
     }
   },
-  height: 420,
+  height: 1100,
   projection: { type: "mill", centralMeridian: 0.0 },
-  width: 500
+  width: 1100
 });
