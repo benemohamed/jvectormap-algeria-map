400498b42e3990ef65eb77a5ef590aadbd434cfe
Mohamed Ben <mohamed@ipfinder.io>
2019-09-02 22:52:32 +0100

    Add Communes de la wilaya mascara

--- src/Wilaya/cities/en/mascara_en.js
+++ src/Wilaya/cities/en/mascara_en.js
@@ -1,9 +1,9 @@
 jQuery.fn.vectorMap("addMap", "mascara_en", {
   insets: [
     {
-      width: 500,
+      width: 1100,
       top: 0,
-      height: 420,
+      height: 1100,
       bbox: [{ y: 0, x: 300 }, { y: 0, x: 0 }],
       left: 0
     }
@@ -11,10 +11,194 @@ jQuery.fn.vectorMap("addMap", "mascara_en", {
   paths: {
     "1": {
       path: "",
-      name: ""
+      name: "Mascara"
+    },
+    "2": {
+      path: "",
+      name: "Bou Hanifia"
+    },
+    "3": {
+      path: "",
+      name: "Tizi"
+    },
+    "4": {
+      path: "",
+      name: "Hacine"
+    },
+    "5": {
+      path: "",
+      name: "Maoussa"
+    },
+    "6": {
+      path: "",
+      name: "Teghennif"
+    },
+    "7": {
+      path: "",
+      name: "El Hachem"
+    },
+    "8": {
+      path: "",
+      name: "Sidi Kada"
+    },
+    "9": {
+      path: "",
+      name: "Zelmata"
+    },
+    "10": {
+      path: "",
+      name: "Oued El Abtal"
+    },
+    "11": {
+      path: "",
+      name: "Ain Ferah"
+    },
+    "12": {
+      path: "",
+      name: "Ghriss"
+    },
+    "13": {
+      path: "",
+      name: "Froha"
+    },
+    "14": {
+      path: "",
+      name: "Matemore"
+    },
+    "15": {
+      path: "",
+      name: "Makdha"
+    },
+    "16": {
+      path: "",
+      name: "Sidi Boussaid"
+    },
+    "17": {
+      path: "",
+      name: "El Bordj"
+    },
+    "18": {
+      path: "",
+      name: "Ain Fekan"
+    },
+    "19": {
+      path: "",
+      name: "Benian"
+    },
+    "20": {
+      path: "",
+      name: "Khalouia"
+    },
+    "21": {
+      path: "",
+      name: "El Menaouer"
+    },
+    "22": {
+      path: "",
+      name: "Oued Taria"
+    },
+    "23": {
+      path: "",
+      name: "Aouf"
+    },
+    "24": {
+      path: "",
+      name: "Ain Fares"
+    },
+    "25": {
+      path: "",
+      name: "Ain Frass"
+    },
+    "26": {
+      path: "",
+      name: "Sig"
+    },
+    "27": {
+      path: "",
+      name: "Oggaz"
+    },
+    "28": {
+      path: "",
+      name: "Alaimia"
+    },
+    "29": {
+      path: "",
+      name: "El Gaada"
+    },
+    "30": {
+      path: "",
+      name: "Zahana"
+    },
+    "31": {
+      path: "",
+      name: "Mohammadia"
+    },
+    "32": {
+      path: "",
+      name: "Sidi Abdelmoumene"
+    },
+    "33": {
+      path: "",
+      name: "Ferraguig"
+    },
+    "34": {
+      path: "",
+      name: "El Ghomri"
+    },
+    "35": {
+      path: "",
+      name: "Sedjerara"
+    },
+    "36": {
+      path: "",
+      name: "Moctadouz"
+    },
+    "37": {
+      path: "",
+      name: "Bou Henni"
+    },
+    "38": {
+      path: "",
+      name: "Guettena"
+    },
+    "39": {
+      path: "",
+      name: "El Mamounia"
+    },
+    "40": {
+      path: "",
+      name: "El Keurt"
+    },
+    "41": {
+      path: "",
+      name: "Gharrous"
+    },
+    "42": {
+      path: "",
+      name: "Gherdjoum"
+    },
+    "43": {
+      path: "",
+      name: "Chorfa"
+    },
+    "44": {
+      path: "",
+      name: "Ras Ain Amirouche"
+    },
+    "45": {
+      path: "",
+      name: "Nesmot"
+    },
+    "46": {
+      path: "",
+      name: "Sidi Abdeldjebar"
+    },
+    "47": {
+      path: "",
+      name: "Sehailia"
     }
   },
-  height: 420,
+  height: 1100,
   projection: { type: "mill", centralMeridian: 0.0 },
-  width: 500
+  width: 1100
 });
