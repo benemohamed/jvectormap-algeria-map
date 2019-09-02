9d731de08a01b3a37560e6fd6f8304e7699dfa1f
Mohamed Ben <mohamed@ipfinder.io>
2019-09-02 22:52:33 +0100

    Add Communes de la wilaya relizane

--- src/Wilaya/cities/en/relizane_en.js
+++ src/Wilaya/cities/en/relizane_en.js
@@ -1,9 +1,9 @@
 jQuery.fn.vectorMap("addMap", "relizane_en", {
   insets: [
     {
-      width: 500,
+      width: 1100,
       top: 0,
-      height: 420,
+      height: 1100,
       bbox: [{ y: 0, x: 300 }, { y: 0, x: 0 }],
       left: 0
     }
@@ -11,10 +11,158 @@ jQuery.fn.vectorMap("addMap", "relizane_en", {
   paths: {
     "1": {
       path: "",
-      name: ""
+      name: "Relizane"
+    },
+    "2": {
+      path: "",
+      name: "Oued Rhiou"
+    },
+    "3": {
+      path: "",
+      name: "Belaassel Bouzegza"
+    },
+    "4": {
+      path: "",
+      name: "Sidi Saada"
+    },
+    "5": {
+      path: "",
+      name: "Ouled Aiche"
+    },
+    "6": {
+      path: "",
+      name: "Sidi Lazreg"
+    },
+    "7": {
+      path: "",
+      name: "El Hamadna"
+    },
+    "8": {
+      path: "",
+      name: "Sidi Mhamed Ben Ali"
+    },
+    "9": {
+      path: "",
+      name: "Mediouna"
+    },
+    "10": {
+      path: "",
+      name: "Sidi Khettab"
+    },
+    "11": {
+      path: "",
+      name: "Ammi Moussa"
+    },
+    "12": {
+      path: "",
+      name: "Zemmoura"
+    },
+    "13": {
+      path: "",
+      name: "Beni Dergoun"
+    },
+    "14": {
+      path: "",
+      name: "Djidiouia"
+    },
+    "15": {
+      path: "",
+      name: "El Guettar"
+    },
+    "16": {
+      path: "",
+      name: "Hamri"
+    },
+    "17": {
+      path: "",
+      name: "El Matmar"
+    },
+    "18": {
+      path: "",
+      name: "Sidi Mhamed Ben Aouda"
+    },
+    "19": {
+      path: "",
+      name: "Ain Tarek"
+    },
+    "20": {
+      path: "",
+      name: "Oued Essalem"
+    },
+    "21": {
+      path: "",
+      name: "Ouarizane"
+    },
+    "22": {
+      path: "",
+      name: "Mazouna"
+    },
+    "23": {
+      path: "",
+      name: "Kalaa"
+    },
+    "24": {
+      path: "",
+      name: "Ain Rahma"
+    },
+    "25": {
+      path: "",
+      name: "Yellel"
+    },
+    "26": {
+      path: "",
+      name: "Oued El Djemaa"
+    },
+    "27": {
+      path: "",
+      name: "Ramka"
+    },
+    "28": {
+      path: "",
+      name: "Mendes"
+    },
+    "29": {
+      path: "",
+      name: "Lahlef"
+    },
+    "30": {
+      path: "",
+      name: "Beni Zentis"
+    },
+    "31": {
+      path: "",
+      name: "Souk El Haad"
+    },
+    "32": {
+      path: "",
+      name: "Dar Ben Abdellah"
+    },
+    "33": {
+      path: "",
+      name: "El Hassi"
+    },
+    "34": {
+      path: "",
+      name: "Had Echkalla"
+    },
+    "35": {
+      path: "",
+      name: "Bendaoud"
+    },
+    "36": {
+      path: "",
+      name: "El Ouldja"
+    },
+    "37": {
+      path: "",
+      name: "Merdja Sidi Abed"
+    },
+    "38": {
+      path: "",
+      name: "Ouled Sidi Mihoub"
     }
   },
-  height: 420,
+  height: 1100,
   projection: { type: "mill", centralMeridian: 0.0 },
-  width: 500
+  width: 1100
 });
