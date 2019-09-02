ede7303b34c7f2f22b4f2b9847504d36a02cd25b
Mohamed Ben <mohamed@ipfinder.io>
2019-09-02 22:52:32 +0100

    Add Communes de la wilaya blida

--- src/Wilaya/cities/en/blida_en.js
+++ src/Wilaya/cities/en/blida_en.js
@@ -1,9 +1,9 @@
 jQuery.fn.vectorMap("addMap", "blida_en", {
   insets: [
     {
-      width: 500,
+      width: 1100,
       top: 0,
-      height: 420,
+      height: 1100,
       bbox: [{ y: 0, x: 300 }, { y: 0, x: 0 }],
       left: 0
     }
@@ -11,10 +11,106 @@ jQuery.fn.vectorMap("addMap", "blida_en", {
   paths: {
     "1": {
       path: "",
-      name: ""
+      name: "Blida"
+    },
+    "2": {
+      path: "",
+      name: "Chebli"
+    },
+    "3": {
+      path: "",
+      name: "Bouinan"
+    },
+    "4": {
+      path: "",
+      name: "Oued El Alleug"
+    },
+    "5": {
+      path: "",
+      name: "Ouled Yaich"
+    },
+    "6": {
+      path: "",
+      name: "Chrea"
+    },
+    "7": {
+      path: "",
+      name: "El Affroun"
+    },
+    "8": {
+      path: "",
+      name: "Chiffa"
+    },
+    "9": {
+      path: "",
+      name: "Hammam Melouane"
+    },
+    "10": {
+      path: "",
+      name: "Ben Khlil"
+    },
+    "11": {
+      path: "",
+      name: "Soumaa"
+    },
+    "12": {
+      path: "",
+      name: "Mouzaia"
+    },
+    "13": {
+      path: "",
+      name: "Souhane"
+    },
+    "14": {
+      path: "",
+      name: "Meftah"
+    },
+    "15": {
+      path: "",
+      name: "Ouled Selama"
+    },
+    "16": {
+      path: "",
+      name: "Boufarik"
+    },
+    "17": {
+      path: "",
+      name: "Larbaa"
+    },
+    "18": {
+      path: "",
+      name: "Oued Djer"
+    },
+    "19": {
+      path: "",
+      name: "Beni Tamou"
+    },
+    "20": {
+      path: "",
+      name: "Bouarfa"
+    },
+    "21": {
+      path: "",
+      name: "Beni Mered"
+    },
+    "22": {
+      path: "",
+      name: "Bougara"
+    },
+    "23": {
+      path: "",
+      name: "Guerrouaou"
+    },
+    "24": {
+      path: "",
+      name: "Ain Romana"
+    },
+    "25": {
+      path: "",
+      name: "Djebabra"
     }
   },
-  height: 420,
+  height: 1100,
   projection: { type: "mill", centralMeridian: 0.0 },
-  width: 500
+  width: 1100
 });
