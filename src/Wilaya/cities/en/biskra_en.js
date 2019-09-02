de939ee42814d8dbddd54d42244fec67da69e864
Mohamed Ben <mohamed@ipfinder.io>
2019-09-02 22:52:31 +0100

    Add Communes de la wilaya biskra

--- src/Wilaya/cities/en/biskra_en.js
+++ src/Wilaya/cities/en/biskra_en.js
@@ -1,9 +1,9 @@
 jQuery.fn.vectorMap("addMap", "biskra_en", {
   insets: [
     {
-      width: 500,
+      width: 1100,
       top: 0,
-      height: 420,
+      height: 1100,
       bbox: [{ y: 0, x: 300 }, { y: 0, x: 0 }],
       left: 0
     }
@@ -11,10 +11,138 @@ jQuery.fn.vectorMap("addMap", "biskra_en", {
   paths: {
     "1": {
       path: "",
-      name: ""
+      name: "Biskra"
+    },
+    "2": {
+      path: "",
+      name: "Oumache"
+    },
+    "3": {
+      path: "",
+      name: "Branis"
+    },
+    "4": {
+      path: "",
+      name: "Chetma"
+    },
+    "5": {
+      path: "",
+      name: "Ouled Djellal"
+    },
+    "6": {
+      path: "",
+      name: "Ras El Miaad"
+    },
+    "7": {
+      path: "",
+      name: "Besbes"
+    },
+    "8": {
+      path: "",
+      name: "Sidi Khaled"
+    },
+    "9": {
+      path: "",
+      name: "Doucen"
+    },
+    "10": {
+      path: "",
+      name: "Ech Chaiba"
+    },
+    "11": {
+      path: "",
+      name: "Sidi Okba"
+    },
+    "12": {
+      path: "",
+      name: "Mchouneche"
+    },
+    "13": {
+      path: "",
+      name: "El Haouch"
+    },
+    "14": {
+      path: "",
+      name: "Ain Naga"
+    },
+    "15": {
+      path: "",
+      name: "Zeribet El Oued"
+    },
+    "16": {
+      path: "",
+      name: "El Feidh"
+    },
+    "17": {
+      path: "",
+      name: "El Kantara"
+    },
+    "18": {
+      path: "",
+      name: "Ain Zaatout"
+    },
+    "19": {
+      path: "",
+      name: "El Outaya"
+    },
+    "20": {
+      path: "",
+      name: "Djemorah"
+    },
+    "21": {
+      path: "",
+      name: "Tolga"
+    },
+    "22": {
+      path: "",
+      name: "Lioua"
+    },
+    "23": {
+      path: "",
+      name: "Lichana"
+    },
+    "24": {
+      path: "",
+      name: "Ourlal"
+    },
+    "25": {
+      path: "",
+      name: "Mlili"
+    },
+    "26": {
+      path: "",
+      name: "Foughala"
+    },
+    "27": {
+      path: "",
+      name: "Bordj Ben Azzouz"
+    },
+    "28": {
+      path: "",
+      name: "Meziraa"
+    },
+    "29": {
+      path: "",
+      name: "Bouchagroun"
+    },
+    "30": {
+      path: "",
+      name: "Mekhadma"
+    },
+    "31": {
+      path: "",
+      name: "El Ghrous"
+    },
+    "32": {
+      path: "",
+      name: "El Hadjab"
+    },
+    "33": {
+      path: "",
+      name: "Khanguet Sidinadji"
     }
   },
-  height: 420,
+  height: 1100,
   projection: { type: "mill", centralMeridian: 0.0 },
-  width: 500
+  width: 1100
 });
