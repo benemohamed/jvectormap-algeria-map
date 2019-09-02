6f0d77ee718f4c57b1001a273453a907c7bb284f
Mohamed Ben <mohamed@ipfinder.io>
2019-09-02 22:52:32 +0100

    Add Communes de la wilaya tiaret

--- src/Wilaya/cities/en/tiaret_en.js
+++ src/Wilaya/cities/en/tiaret_en.js
@@ -1,9 +1,9 @@
 jQuery.fn.vectorMap("addMap", "tiaret_en", {
   insets: [
     {
-      width: 500,
+      width: 1100,
       top: 0,
-      height: 420,
+      height: 1100,
       bbox: [{ y: 0, x: 300 }, { y: 0, x: 0 }],
       left: 0
     }
@@ -11,10 +11,174 @@ jQuery.fn.vectorMap("addMap", "tiaret_en", {
   paths: {
     "1": {
       path: "",
-      name: ""
+      name: "Tiaret"
+    },
+    "2": {
+      path: "",
+      name: "Medroussa"
+    },
+    "3": {
+      path: "",
+      name: "Ain Bouchekif"
+    },
+    "4": {
+      path: "",
+      name: "Sidi Ali Mellal"
+    },
+    "5": {
+      path: "",
+      name: "Ain Zarit"
+    },
+    "6": {
+      path: "",
+      name: "Ain Deheb"
+    },
+    "7": {
+      path: "",
+      name: "Sidi Bakhti"
+    },
+    "8": {
+      path: "",
+      name: "Medrissa"
+    },
+    "9": {
+      path: "",
+      name: "Zmalet El Emir Aek"
+    },
+    "10": {
+      path: "",
+      name: "Madna"
+    },
+    "11": {
+      path: "",
+      name: "Sebt"
+    },
+    "12": {
+      path: "",
+      name: "Mellakou"
+    },
+    "13": {
+      path: "",
+      name: "Dahmouni"
+    },
+    "14": {
+      path: "",
+      name: "Rahouia"
+    },
+    "15": {
+      path: "",
+      name: "Mahdia"
+    },
+    "16": {
+      path: "",
+      name: "Sougueur"
+    },
+    "17": {
+      path: "",
+      name: "Sidi Abdelghani"
+    },
+    "18": {
+      path: "",
+      name: "Ain El Hadid"
+    },
+    "19": {
+      path: "",
+      name: "Ouled Djerad"
+    },
+    "20": {
+      path: "",
+      name: "Naima"
+    },
+    "21": {
+      path: "",
+      name: "Meghila"
+    },
+    "22": {
+      path: "",
+      name: "Guertoufa"
+    },
+    "23": {
+      path: "",
+      name: "Sidi Hosni"
+    },
+    "24": {
+      path: "",
+      name: "Djillali Ben Amar"
+    },
+    "25": {
+      path: "",
+      name: "Sebaine"
+    },
+    "26": {
+      path: "",
+      name: "Tousnina"
+    },
+    "27": {
+      path: "",
+      name: "Frenda"
+    },
+    "28": {
+      path: "",
+      name: "Ain Kermes"
+    },
+    "29": {
+      path: "",
+      name: "Ksar Chellala"
+    },
+    "30": {
+      path: "",
+      name: "Rechaiga"
+    },
+    "31": {
+      path: "",
+      name: "Nadorah"
+    },
+    "32": {
+      path: "",
+      name: "Tagdemt"
+    },
+    "33": {
+      path: "",
+      name: "Oued Lilli"
+    },
+    "34": {
+      path: "",
+      name: "Mechraa Safa"
+    },
+    "35": {
+      path: "",
+      name: "Hamadia"
+    },
+    "36": {
+      path: "",
+      name: "Chehaima"
+    },
+    "37": {
+      path: "",
+      name: "Takhemaret"
+    },
+    "38": {
+      path: "",
+      name: "Sidi Abderrahmane"
+    },
+    "39": {
+      path: "",
+      name: "Serghine"
+    },
+    "40": {
+      path: "",
+      name: "Bougara"
+    },
+    "41": {
+      path: "",
+      name: "Faidja"
+    },
+    "42": {
+      path: "",
+      name: "Tidda"
     }
   },
-  height: 420,
+  height: 1100,
   projection: { type: "mill", centralMeridian: 0.0 },
-  width: 500
+  width: 1100
 });
