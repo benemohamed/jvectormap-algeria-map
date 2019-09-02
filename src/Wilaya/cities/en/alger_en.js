76a882b1c1d612074a8d8b8615112f65c4ef5d46
Mohamed Ben <mohamed@ipfinder.io>
2019-09-02 22:52:32 +0100

    Add Communes de la wilaya alger

--- src/Wilaya/cities/en/alger_en.js
+++ src/Wilaya/cities/en/alger_en.js
@@ -1,9 +1,9 @@
 jQuery.fn.vectorMap("addMap", "alger_en", {
   insets: [
     {
-      width: 500,
+      width: 1100,
       top: 0,
-      height: 420,
+      height: 1100,
       bbox: [{ y: 0, x: 300 }, { y: 0, x: 0 }],
       left: 0
     }
@@ -11,10 +11,234 @@ jQuery.fn.vectorMap("addMap", "alger_en", {
   paths: {
     "1": {
       path: "",
-      name: ""
+      name: "Alger Centre"
+    },
+    "2": {
+      path: "",
+      name: "Sidi Mhamed"
+    },
+    "3": {
+      path: "",
+      name: "El Madania"
+    },
+    "4": {
+      path: "",
+      name: "Hamma Anassers"
+    },
+    "5": {
+      path: "",
+      name: "Bab El Oued"
+    },
+    "6": {
+      path: "",
+      name: "Bologhine Ibn Ziri"
+    },
+    "7": {
+      path: "",
+      name: "Casbah"
+    },
+    "8": {
+      path: "",
+      name: "Oued Koriche"
+    },
+    "9": {
+      path: "",
+      name: "Bir Mourad Rais"
+    },
+    "10": {
+      path: "",
+      name: "El Biar"
+    },
+    "11": {
+      path: "",
+      name: "Bouzareah"
+    },
+    "12": {
+      path: "",
+      name: "Birkhadem"
+    },
+    "13": {
+      path: "",
+      name: "El Harrach"
+    },
+    "14": {
+      path: "",
+      name: "Baraki"
+    },
+    "15": {
+      path: "",
+      name: "Oued Smar"
+    },
+    "16": {
+      path: "",
+      name: "Bourouba"
+    },
+    "17": {
+      path: "",
+      name: "Hussein Dey"
+    },
+    "18": {
+      path: "",
+      name: "Kouba"
+    },
+    "19": {
+      path: "",
+      name: "Bachedjerah"
+    },
+    "20": {
+      path: "",
+      name: "Dar El Beida"
+    },
+    "21": {
+      path: "",
+      name: "Bab Azzouar"
+    },
+    "22": {
+      path: "",
+      name: "Ben Aknoun"
+    },
+    "23": {
+      path: "",
+      name: "Dely Ibrahim"
+    },
+    "24": {
+      path: "",
+      name: "Bains Romains"
+    },
+    "25": {
+      path: "",
+      name: "Rais Hamidou"
+    },
+    "26": {
+      path: "",
+      name: "Djasr Kasentina"
+    },
+    "27": {
+      path: "",
+      name: "El Mouradia"
+    },
+    "28": {
+      path: "",
+      name: "Hydra"
+    },
+    "29": {
+      path: "",
+      name: "Mohammadia"
+    },
+    "30": {
+      path: "",
+      name: "Bordj El Kiffan"
+    },
+    "31": {
+      path: "",
+      name: "El Magharia"
+    },
+    "32": {
+      path: "",
+      name: "Beni Messous"
+    },
+    "33": {
+      path: "",
+      name: "Les Eucalyptus"
+    },
+    "34": {
+      path: "",
+      name: "Birtouta"
+    },
+    "35": {
+      path: "",
+      name: "Tassala El Merdja"
+    },
+    "36": {
+      path: "",
+      name: "Ouled Chebel"
+    },
+    "37": {
+      path: "",
+      name: "Sidi Moussa"
+    },
+    "38": {
+      path: "",
+      name: "Ain Taya"
+    },
+    "39": {
+      path: "",
+      name: "Bordj El Bahri"
+    },
+    "40": {
+      path: "",
+      name: "Marsa"
+    },
+    "41": {
+      path: "",
+      name: "Haraoua"
+    },
+    "42": {
+      path: "",
+      name: "Rouiba"
+    },
+    "43": {
+      path: "",
+      name: "Reghaia"
+    },
+    "44": {
+      path: "",
+      name: "Ain Benian"
+    },
+    "45": {
+      path: "",
+      name: "Staoueli"
+    },
+    "46": {
+      path: "",
+      name: "Zeralda"
+    },
+    "47": {
+      path: "",
+      name: "Mahelma"
+    },
+    "48": {
+      path: "",
+      name: "Rahmania"
+    },
+    "49": {
+      path: "",
+      name: "Souidania"
+    },
+    "50": {
+      path: "",
+      name: "Cheraga"
+    },
+    "51": {
+      path: "",
+      name: "Ouled Fayet"
+    },
+    "52": {
+      path: "",
+      name: "El Achour"
+    },
+    "53": {
+      path: "",
+      name: "Draria"
+    },
+    "54": {
+      path: "",
+      name: "Douera"
+    },
+    "55": {
+      path: "",
+      name: "Baba Hassen"
+    },
+    "56": {
+      path: "",
+      name: "Khracia"
+    },
+    "57": {
+      path: "",
+      name: "Saoula"
     }
   },
-  height: 420,
+  height: 1100,
   projection: { type: "mill", centralMeridian: 0.0 },
-  width: 500
+  width: 1100
 });
