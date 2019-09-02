41624e386c9139de19a7ad0da2b2e362d8fcd475
Mohamed Ben <mohamed@ipfinder.io>
2019-09-02 22:52:32 +0100

    Add Communes de la wilaya guelma

--- src/Wilaya/cities/en/guelma_en.js
+++ src/Wilaya/cities/en/guelma_en.js
@@ -1,9 +1,9 @@
 jQuery.fn.vectorMap("addMap", "guelma_en", {
   insets: [
     {
-      width: 500,
+      width: 1100,
       top: 0,
-      height: 420,
+      height: 1100,
       bbox: [{ y: 0, x: 300 }, { y: 0, x: 0 }],
       left: 0
     }
@@ -11,10 +11,142 @@ jQuery.fn.vectorMap("addMap", "guelma_en", {
   paths: {
     "1": {
       path: "",
-      name: ""
+      name: "Guelma"
+    },
+    "2": {
+      path: "",
+      name: "Nechmaya"
+    },
+    "3": {
+      path: "",
+      name: "Bouati Mahmoud"
+    },
+    "4": {
+      path: "",
+      name: "Oued Zenati"
+    },
+    "5": {
+      path: "",
+      name: "Tamlouka"
+    },
+    "6": {
+      path: "",
+      name: "Oued Fragha"
+    },
+    "7": {
+      path: "",
+      name: "Ain Sandel"
+    },
+    "8": {
+      path: "",
+      name: "Ras El Agba"
+    },
+    "9": {
+      path: "",
+      name: "Dahouara"
+    },
+    "10": {
+      path: "",
+      name: "Belkhir"
+    },
+    "11": {
+      path: "",
+      name: "Ben Djarah"
+    },
+    "12": {
+      path: "",
+      name: "Bou Hamdane"
+    },
+    "13": {
+      path: "",
+      name: "Ain Makhlouf"
+    },
+    "14": {
+      path: "",
+      name: "Ain Ben Beida"
+    },
+    "15": {
+      path: "",
+      name: "Khezara"
+    },
+    "16": {
+      path: "",
+      name: "Beni Mezline"
+    },
+    "17": {
+      path: "",
+      name: "Bou Hachana"
+    },
+    "18": {
+      path: "",
+      name: "Guelaat Bou Sbaa"
+    },
+    "19": {
+      path: "",
+      name: "Hammam Maskhoutine"
+    },
+    "20": {
+      path: "",
+      name: "El Fedjoudj"
+    },
+    "21": {
+      path: "",
+      name: "Bordj Sabat"
+    },
+    "22": {
+      path: "",
+      name: "Hamman Nbail"
+    },
+    "23": {
+      path: "",
+      name: "Ain Larbi"
+    },
+    "24": {
+      path: "",
+      name: "Medjez Amar"
+    },
+    "25": {
+      path: "",
+      name: "Bouchegouf"
+    },
+    "26": {
+      path: "",
+      name: "Heliopolis"
+    },
+    "27": {
+      path: "",
+      name: "Ain Hessania"
+    },
+    "28": {
+      path: "",
+      name: "Roknia"
+    },
+    "29": {
+      path: "",
+      name: "Salaoua Announa"
+    },
+    "30": {
+      path: "",
+      name: "Medjez Sfa"
+    },
+    "31": {
+      path: "",
+      name: "Boumahra Ahmed"
+    },
+    "32": {
+      path: "",
+      name: "Ain Reggada"
+    },
+    "33": {
+      path: "",
+      name: "Oued Cheham"
+    },
+    "34": {
+      path: "",
+      name: "Djeballah Khemissi"
     }
   },
-  height: 420,
+  height: 1100,
   projection: { type: "mill", centralMeridian: 0.0 },
-  width: 500
+  width: 1100
 });
