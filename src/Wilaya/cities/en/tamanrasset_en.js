6f89f3d590db2e054be954a7bbf773c95f08845d
Mohamed Ben <mohamed@ipfinder.io>
2019-09-02 22:52:32 +0100

    Add Communes de la wilaya tamanrasset

--- src/Wilaya/cities/en/tamanrasset_en.js
+++ src/Wilaya/cities/en/tamanrasset_en.js
@@ -1,9 +1,9 @@
 jQuery.fn.vectorMap("addMap", "tamanrasset_en", {
   insets: [
     {
-      width: 500,
+      width: 1100,
       top: 0,
-      height: 420,
+      height: 1100,
       bbox: [{ y: 0, x: 300 }, { y: 0, x: 0 }],
       left: 0
     }
@@ -11,10 +11,46 @@ jQuery.fn.vectorMap("addMap", "tamanrasset_en", {
   paths: {
     "1": {
       path: "",
-      name: ""
+      name: "Tamanghasset"
+    },
+    "2": {
+      path: "",
+      name: "Abalessa"
+    },
+    "3": {
+      path: "",
+      name: "In Ghar"
+    },
+    "4": {
+      path: "",
+      name: "In Guezzam"
+    },
+    "5": {
+      path: "",
+      name: "Idles"
+    },
+    "6": {
+      path: "",
+      name: "Tazouk"
+    },
+    "7": {
+      path: "",
+      name: "Tinzaouatine"
+    },
+    "8": {
+      path: "",
+      name: "In Salah"
+    },
+    "9": {
+      path: "",
+      name: "In Amguel"
+    },
+    "10": {
+      path: "",
+      name: "Foggaret Ezzaouia"
     }
   },
-  height: 420,
+  height: 1100,
   projection: { type: "mill", centralMeridian: 0.0 },
-  width: 500
+  width: 1100
 });
