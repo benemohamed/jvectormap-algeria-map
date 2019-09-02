03d3329cec2bbf26f6af706c5153cc786f4c1d34
Mohamed Ben <mohamed@ipfinder.io>
2019-09-02 22:52:32 +0100

    Add Communes de la wilaya tizi_ouzou

--- src/Wilaya/cities/en/tizi_ouzou_en.js
+++ src/Wilaya/cities/en/tizi_ouzou_en.js
@@ -1,9 +1,9 @@
 jQuery.fn.vectorMap("addMap", "tizi_ouzou_en", {
   insets: [
     {
-      width: 500,
+      width: 1100,
       top: 0,
-      height: 420,
+      height: 1100,
       bbox: [{ y: 0, x: 300 }, { y: 0, x: 0 }],
       left: 0
     }
@@ -11,10 +11,274 @@ jQuery.fn.vectorMap("addMap", "tizi_ouzou_en", {
   paths: {
     "1": {
       path: "",
-      name: ""
+      name: "Tizi Ouzou"
+    },
+    "2": {
+      path: "",
+      name: "Ain El Hammam"
+    },
+    "3": {
+      path: "",
+      name: "Akbil"
+    },
+    "4": {
+      path: "",
+      name: "Freha"
+    },
+    "5": {
+      path: "",
+      name: "Souamaa"
+    },
+    "6": {
+      path: "",
+      name: "Mechtrass"
+    },
+    "7": {
+      path: "",
+      name: "Irdjen"
+    },
+    "8": {
+      path: "",
+      name: "Timizart"
+    },
+    "9": {
+      path: "",
+      name: "Makouda"
+    },
+    "10": {
+      path: "",
+      name: "Draa El Mizan"
+    },
+    "11": {
+      path: "",
+      name: "Tizi Ghenif"
+    },
+    "12": {
+      path: "",
+      name: "Bounouh"
+    },
+    "13": {
+      path: "",
+      name: "Ait Chaffaa"
+    },
+    "14": {
+      path: "",
+      name: "Frikat"
+    },
+    "15": {
+      path: "",
+      name: "Beni Aissi"
+    },
+    "16": {
+      path: "",
+      name: "Beni Zmenzer"
+    },
+    "17": {
+      path: "",
+      name: "Iferhounene"
+    },
+    "18": {
+      path: "",
+      name: "Azazga"
+    },
+    "19": {
+      path: "",
+      name: "Iloula Oumalou"
+    },
+    "20": {
+      path: "",
+      name: "Yakouren"
+    },
+    "21": {
+      path: "",
+      name: "Larba Nait Irathen"
+    },
+    "22": {
+      path: "",
+      name: "Tizi Rached"
+    },
+    "23": {
+      path: "",
+      name: "Zekri"
+    },
+    "24": {
+      path: "",
+      name: "Ouaguenoun"
+    },
+    "25": {
+      path: "",
+      name: "Ain Zaouia"
+    },
+    "26": {
+      path: "",
+      name: "Mkira"
+    },
+    "27": {
+      path: "",
+      name: "Ait Yahia"
+    },
+    "28": {
+      path: "",
+      name: "Ait Mahmoud"
+    },
+    "29": {
+      path: "",
+      name: "Maatka"
+    },
+    "30": {
+      path: "",
+      name: "Ait Boumehdi"
+    },
+    "31": {
+      path: "",
+      name: "Abi Youcef"
+    },
+    "32": {
+      path: "",
+      name: "Beni Douala"
+    },
+    "33": {
+      path: "",
+      name: "Illilten"
+    },
+    "34": {
+      path: "",
+      name: "Bouzguen"
+    },
+    "35": {
+      path: "",
+      name: "Ait Aggouacha"
+    },
+    "36": {
+      path: "",
+      name: "Ouadhia"
+    },
+    "37": {
+      path: "",
+      name: "Azzefoun"
+    },
+    "38": {
+      path: "",
+      name: "Tigzirt"
+    },
+    "39": {
+      path: "",
+      name: "Ait Aissa Mimoun"
+    },
+    "40": {
+      path: "",
+      name: "Boghni"
+    },
+    "41": {
+      path: "",
+      name: "Ifigha"
+    },
+    "42": {
+      path: "",
+      name: "Ait Oumalou"
+    },
+    "43": {
+      path: "",
+      name: "Tirmitine"
+    },
+    "44": {
+      path: "",
+      name: "Akerrou"
+    },
+    "45": {
+      path: "",
+      name: "Yatafen"
+    },
+    "46": {
+      path: "",
+      name: "Beni Ziki"
+    },
+    "47": {
+      path: "",
+      name: "Draa Ben Khedda"
+    },
+    "48": {
+      path: "",
+      name: "Ouacif"
+    },
+    "49": {
+      path: "",
+      name: "Idjeur"
+    },
+    "50": {
+      path: "",
+      name: "Mekla"
+    },
+    "51": {
+      path: "",
+      name: "Tizi Nthlata"
+    },
+    "52": {
+      path: "",
+      name: "Beni Yenni"
+    },
+    "53": {
+      path: "",
+      name: "Aghrib"
+    },
+    "54": {
+      path: "",
+      name: "Iflissen"
+    },
+    "55": {
+      path: "",
+      name: "Boudjima"
+    },
+    "56": {
+      path: "",
+      name: "Ait Yahia Moussa"
+    },
+    "57": {
+      path: "",
+      name: "Souk El Thenine"
+    },
+    "58": {
+      path: "",
+      name: "Ait Khelil"
+    },
+    "59": {
+      path: "",
+      name: "Sidi Naamane"
+    },
+    "60": {
+      path: "",
+      name: "Iboudraren"
+    },
+    "61": {
+      path: "",
+      name: "Aghni Goughran"
+    },
+    "62": {
+      path: "",
+      name: "Mizrana"
+    },
+    "63": {
+      path: "",
+      name: "Imsouhal"
+    },
+    "64": {
+      path: "",
+      name: "Tadmait"
+    },
+    "65": {
+      path: "",
+      name: "Ait Bouadou"
+    },
+    "66": {
+      path: "",
+      name: "Assi Youcef"
+    },
+    "67": {
+      path: "",
+      name: "Ait Toudert"
     }
   },
-  height: 420,
+  height: 1100,
   projection: { type: "mill", centralMeridian: 0.0 },
-  width: 500
+  width: 1100
 });
