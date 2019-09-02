0fd0273a2001f3caaff48c62576414bee3ea2f4e
Mohamed Ben <mohamed@ipfinder.io>
2019-09-02 22:52:32 +0100

    Add Communes de la wilaya m_sila

--- src/Wilaya/cities/en/m_sila_en.js
+++ src/Wilaya/cities/en/m_sila_en.js
@@ -1,9 +1,9 @@
 jQuery.fn.vectorMap("addMap", "m_sila_en", {
   insets: [
     {
-      width: 500,
+      width: 1100,
       top: 0,
-      height: 420,
+      height: 1100,
       bbox: [{ y: 0, x: 300 }, { y: 0, x: 0 }],
       left: 0
     }
@@ -11,10 +11,194 @@ jQuery.fn.vectorMap("addMap", "m_sila_en", {
   paths: {
     "1": {
       path: "",
-      name: ""
+      name: "Msila"
+    },
+    "2": {
+      path: "",
+      name: "Maadid"
+    },
+    "3": {
+      path: "",
+      name: "Hammam Dhalaa"
+    },
+    "4": {
+      path: "",
+      name: "Ouled Derradj"
+    },
+    "5": {
+      path: "",
+      name: "Tarmount"
+    },
+    "6": {
+      path: "",
+      name: "Mtarfa"
+    },
+    "7": {
+      path: "",
+      name: "Khoubana"
+    },
+    "8": {
+      path: "",
+      name: "Mcif"
+    },
+    "9": {
+      path: "",
+      name: "Chellal"
+    },
+    "10": {
+      path: "",
+      name: "Ouled Madhi"
+    },
+    "11": {
+      path: "",
+      name: "Magra"
+    },
+    "12": {
+      path: "",
+      name: "Berhoum"
+    },
+    "13": {
+      path: "",
+      name: "Ain Khadra"
+    },
+    "14": {
+      path: "",
+      name: "Ouled Addi Guebala"
+    },
+    "15": {
+      path: "",
+      name: "Belaiba"
+    },
+    "16": {
+      path: "",
+      name: "Sidi Aissa"
+    },
+    "17": {
+      path: "",
+      name: "Ain El Hadjel"
+    },
+    "18": {
+      path: "",
+      name: "Sidi Hadjeres"
+    },
+    "19": {
+      path: "",
+      name: "Ouanougha"
+    },
+    "20": {
+      path: "",
+      name: "Bou Saada"
+    },
+    "21": {
+      path: "",
+      name: "Ouled Sidi Brahim"
+    },
+    "22": {
+      path: "",
+      name: "Sidi Ameur"
+    },
+    "23": {
+      path: "",
+      name: "Tamsa"
+    },
+    "24": {
+      path: "",
+      name: "Ben Srour"
+    },
+    "25": {
+      path: "",
+      name: "Ouled Slimane"
+    },
+    "26": {
+      path: "",
+      name: "El Houamed"
+    },
+    "27": {
+      path: "",
+      name: "El Hamel"
+    },
+    "28": {
+      path: "",
+      name: "Ouled Mansour"
+    },
+    "29": {
+      path: "",
+      name: "Maarif"
+    },
+    "30": {
+      path: "",
+      name: "Dehahna"
+    },
+    "31": {
+      path: "",
+      name: "Bouti Sayah"
+    },
+    "32": {
+      path: "",
+      name: "Khettouti Sed Djir"
+    },
+    "33": {
+      path: "",
+      name: "Zarzour"
+    },
+    "34": {
+      path: "",
+      name: "Oued Chair"
+    },
+    "35": {
+      path: "",
+      name: "Benzouh"
+    },
+    "36": {
+      path: "",
+      name: "Bir Foda"
+    },
+    "37": {
+      path: "",
+      name: "Ain Fares"
+    },
+    "38": {
+      path: "",
+      name: "Sidi Mhamed"
+    },
+    "39": {
+      path: "",
+      name: "Ouled Atia"
+    },
+    "40": {
+      path: "",
+      name: "Souamaa"
+    },
+    "41": {
+      path: "",
+      name: "Ain El Melh"
+    },
+    "42": {
+      path: "",
+      name: "Medjedel"
+    },
+    "43": {
+      path: "",
+      name: "Slim"
+    },
+    "44": {
+      path: "",
+      name: "Ain Errich"
+    },
+    "45": {
+      path: "",
+      name: "Beni Ilmane"
+    },
+    "46": {
+      path: "",
+      name: "Oultene"
+    },
+    "47": {
+      path: "",
+      name: "Djebel Messaad"
     }
   },
-  height: 420,
+  height: 1100,
   projection: { type: "mill", centralMeridian: 0.0 },
-  width: 500
+  width: 1100
 });
