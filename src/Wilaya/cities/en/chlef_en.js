ae5ef4c1408f427b82c608aeac68f3700eb6ec40
Mohamed Ben <mohamed@ipfinder.io>
2019-09-02 22:52:31 +0100

    Add Communes de la wilaya chlef

--- src/Wilaya/cities/en/chlef_en.js
+++ src/Wilaya/cities/en/chlef_en.js
@@ -1,9 +1,9 @@
 jQuery.fn.vectorMap("addMap", "chlef_en", {
   insets: [
     {
-      width: 500,
+      width: 1100,
       top: 0,
-      height: 420,
+      height: 1100,
       bbox: [{ y: 0, x: 300 }, { y: 0, x: 0 }],
       left: 0
     }
@@ -11,10 +11,146 @@ jQuery.fn.vectorMap("addMap", "chlef_en", {
   paths: {
     "1": {
       path: "",
-      name: ""
+      name: "Chlef"
+    },
+    "2": {
+      path: "",
+      name: "Tenes"
+    },
+    "3": {
+      path: "",
+      name: "Benairia"
+    },
+    "4": {
+      path: "",
+      name: "El Karimia"
+    },
+    "5": {
+      path: "",
+      name: "Tadjna"
+    },
+    "6": {
+      path: "",
+      name: "Taougrite"
+    },
+    "7": {
+      path: "",
+      name: "Beni Haoua"
+    },
+    "8": {
+      path: "",
+      name: "Sobha"
+    },
+    "9": {
+      path: "",
+      name: "Harchoun"
+    },
+    "10": {
+      path: "",
+      name: "Ouled Fares"
+    },
+    "11": {
+      path: "",
+      name: "Sidi Akacha"
+    },
+    "12": {
+      path: "",
+      name: "Boukadir"
+    },
+    "13": {
+      path: "",
+      name: "Beni Rached"
+    },
+    "14": {
+      path: "",
+      name: "Talassa"
+    },
+    "15": {
+      path: "",
+      name: "Herenfa"
+    },
+    "16": {
+      path: "",
+      name: "Oued Goussine"
+    },
+    "17": {
+      path: "",
+      name: "Dahra"
+    },
+    "18": {
+      path: "",
+      name: "Ouled Abbes"
+    },
+    "19": {
+      path: "",
+      name: "Sendjas"
+    },
+    "20": {
+      path: "",
+      name: "Zeboudja"
+    },
+    "21": {
+      path: "",
+      name: "Oued Sly"
+    },
+    "22": {
+      path: "",
+      name: "Abou El Hassen"
+    },
+    "23": {
+      path: "",
+      name: "El Marsa"
+    },
+    "24": {
+      path: "",
+      name: "Chettia"
+    },
+    "25": {
+      path: "",
+      name: "Sidi Abderrahmane"
+    },
+    "26": {
+      path: "",
+      name: "Moussadek"
+    },
+    "27": {
+      path: "",
+      name: "El Hadjadj"
+    },
+    "28": {
+      path: "",
+      name: "Labiod Medjadja"
+    },
+    "29": {
+      path: "",
+      name: "Oued Fodda"
+    },
+    "30": {
+      path: "",
+      name: "Ouled Ben Abdelkader"
+    },
+    "31": {
+      path: "",
+      name: "Bouzghaia"
+    },
+    "32": {
+      path: "",
+      name: "Ain Merane"
+    },
+    "33": {
+      path: "",
+      name: "Oum Drou"
+    },
+    "34": {
+      path: "",
+      name: "Breira"
+    },
+    "35": {
+      path: "",
+      name: "Ben Boutaleb"
     }
   },
-  height: 420,
+  height: 1100,
   projection: { type: "mill", centralMeridian: 0.0 },
-  width: 500
+  width: 1100
 });
