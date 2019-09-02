eda1b7aca84646aa3d437248b6611e135ea9826c
Mohamed Ben <mohamed@ipfinder.io>
2019-09-02 22:52:32 +0100

    Add Communes de la wilaya medea

--- src/Wilaya/cities/en/medea_en.js
+++ src/Wilaya/cities/en/medea_en.js
@@ -1,9 +1,9 @@
 jQuery.fn.vectorMap("addMap", "medea_en", {
   insets: [
     {
-      width: 500,
+      width: 1100,
       top: 0,
-      height: 420,
+      height: 1100,
       bbox: [{ y: 0, x: 300 }, { y: 0, x: 0 }],
       left: 0
     }
@@ -11,10 +11,262 @@ jQuery.fn.vectorMap("addMap", "medea_en", {
   paths: {
     "1": {
       path: "",
-      name: ""
+      name: "Medea"
+    },
+    "2": {
+      path: "",
+      name: "Ouzera"
+    },
+    "3": {
+      path: "",
+      name: "Ouled Maaref"
+    },
+    "4": {
+      path: "",
+      name: "Ain Boucif"
+    },
+    "5": {
+      path: "",
+      name: "Aissaouia"
+    },
+    "6": {
+      path: "",
+      name: "Ouled Deide"
+    },
+    "7": {
+      path: "",
+      name: "El Omaria"
+    },
+    "8": {
+      path: "",
+      name: "Derrag"
+    },
+    "9": {
+      path: "",
+      name: "El Guelbelkebir"
+    },
+    "10": {
+      path: "",
+      name: "Bouaiche"
+    },
+    "11": {
+      path: "",
+      name: "Mezerena"
+    },
+    "12": {
+      path: "",
+      name: "Ouled Brahim"
+    },
+    "13": {
+      path: "",
+      name: "Damiat"
+    },
+    "14": {
+      path: "",
+      name: "Sidi Ziane"
+    },
+    "15": {
+      path: "",
+      name: "Tamesguida"
+    },
+    "16": {
+      path: "",
+      name: "El Hamdania"
+    },
+    "17": {
+      path: "",
+      name: "Kef Lakhdar"
+    },
+    "18": {
+      path: "",
+      name: "Chelalet El Adhaoura"
+    },
+    "19": {
+      path: "",
+      name: "Bouskene"
+    },
+    "20": {
+      path: "",
+      name: "Rebaia"
+    },
+    "21": {
+      path: "",
+      name: "Bouchrahil"
+    },
+    "22": {
+      path: "",
+      name: "Ouled Hellal"
+    },
+    "23": {
+      path: "",
+      name: "Tafraout"
+    },
+    "24": {
+      path: "",
+      name: "Baata"
+    },
+    "25": {
+      path: "",
+      name: "Boghar"
+    },
+    "26": {
+      path: "",
+      name: "Sidi Naamane"
+    },
+    "27": {
+      path: "",
+      name: "Ouled Bouachra"
+    },
+    "28": {
+      path: "",
+      name: "Sidi Zahar"
+    },
+    "29": {
+      path: "",
+      name: "Oued Harbil"
+    },
+    "30": {
+      path: "",
+      name: "Benchicao"
+    },
+    "31": {
+      path: "",
+      name: "Sidi Damed"
+    },
+    "32": {
+      path: "",
+      name: "Aziz"
+    },
+    "33": {
+      path: "",
+      name: "Souagui"
+    },
+    "34": {
+      path: "",
+      name: "Zoubiria"
+    },
+    "35": {
+      path: "",
+      name: "Ksar El Boukhari"
+    },
+    "36": {
+      path: "",
+      name: "El Azizia"
+    },
+    "37": {
+      path: "",
+      name: "Djouab"
+    },
+    "38": {
+      path: "",
+      name: "Chahbounia"
+    },
+    "39": {
+      path: "",
+      name: "Meghraoua"
+    },
+    "40": {
+      path: "",
+      name: "Cheniguel"
+    },
+    "41": {
+      path: "",
+      name: "Ain Ouksir"
+    },
+    "42": {
+      path: "",
+      name: "Oum El Djalil"
+    },
+    "43": {
+      path: "",
+      name: "Ouamri"
+    },
+    "44": {
+      path: "",
+      name: "Si Mahdjoub"
+    },
+    "45": {
+      path: "",
+      name: "Tlatet Eddoair"
+    },
+    "46": {
+      path: "",
+      name: "Beni Slimane"
+    },
+    "47": {
+      path: "",
+      name: "Berrouaghia"
+    },
+    "48": {
+      path: "",
+      name: "Seghouane"
+    },
+    "49": {
+      path: "",
+      name: "Meftaha"
+    },
+    "50": {
+      path: "",
+      name: "Mihoub"
+    },
+    "51": {
+      path: "",
+      name: "Boughezoul"
+    },
+    "52": {
+      path: "",
+      name: "Tablat"
+    },
+    "53": {
+      path: "",
+      name: "Deux Bassins"
+    },
+    "54": {
+      path: "",
+      name: "Draa Essamar"
+    },
+    "55": {
+      path: "",
+      name: "Sidi Errabia"
+    },
+    "56": {
+      path: "",
+      name: "Bir Ben Laabed"
+    },
+    "57": {
+      path: "",
+      name: "El Ouinet"
+    },
+    "58": {
+      path: "",
+      name: "Ouled Antar"
+    },
+    "59": {
+      path: "",
+      name: "Bouaichoune"
+    },
+    "60": {
+      path: "",
+      name: "Hannacha"
+    },
+    "61": {
+      path: "",
+      name: "Sedraia"
+    },
+    "62": {
+      path: "",
+      name: "Medjebar"
+    },
+    "63": {
+      path: "",
+      name: "Khams Djouamaa"
+    },
+    "64": {
+      path: "",
+      name: "Saneg"
     }
   },
-  height: 420,
+  height: 1100,
   projection: { type: "mill", centralMeridian: 0.0 },
-  width: 500
+  width: 1100
 });
