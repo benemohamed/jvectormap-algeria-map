0b9e25b7dfceccb39d54c42aedd38f48e8206400
Mohamed Ben <mohamed@ipfinder.io>
2019-09-02 22:52:32 +0100

    Add Communes de la wilaya annaba

--- src/Wilaya/cities/en/annaba_en.js
+++ src/Wilaya/cities/en/annaba_en.js
@@ -1,9 +1,9 @@
 jQuery.fn.vectorMap("addMap", "annaba_en", {
   insets: [
     {
-      width: 500,
+      width: 1100,
       top: 0,
-      height: 420,
+      height: 1100,
       bbox: [{ y: 0, x: 300 }, { y: 0, x: 0 }],
       left: 0
     }
@@ -11,10 +11,54 @@ jQuery.fn.vectorMap("addMap", "annaba_en", {
   paths: {
     "1": {
       path: "",
-      name: ""
+      name: "Annaba"
+    },
+    "2": {
+      path: "",
+      name: "Berrahel"
+    },
+    "3": {
+      path: "",
+      name: "El Hadjar"
+    },
+    "4": {
+      path: "",
+      name: "Eulma"
+    },
+    "5": {
+      path: "",
+      name: "El Bouni"
+    },
+    "6": {
+      path: "",
+      name: "Oued El Aneb"
+    },
+    "7": {
+      path: "",
+      name: "Cheurfa"
+    },
+    "8": {
+      path: "",
+      name: "Seraidi"
+    },
+    "9": {
+      path: "",
+      name: "Ain Berda"
+    },
+    "10": {
+      path: "",
+      name: "Chetaibi"
+    },
+    "11": {
+      path: "",
+      name: "Sidi Amer"
+    },
+    "12": {
+      path: "",
+      name: "Treat"
     }
   },
-  height: 420,
+  height: 1100,
   projection: { type: "mill", centralMeridian: 0.0 },
-  width: 500
+  width: 1100
 });
