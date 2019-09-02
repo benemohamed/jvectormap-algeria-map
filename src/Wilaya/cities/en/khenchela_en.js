4f3bd783fc024904d7b238ca062ef75a630c127c
Mohamed Ben <mohamed@ipfinder.io>
2019-09-02 22:52:33 +0100

    Add Communes de la wilaya khenchela

--- src/Wilaya/cities/en/khenchela_en.js
+++ src/Wilaya/cities/en/khenchela_en.js
@@ -1,9 +1,9 @@
 jQuery.fn.vectorMap("addMap", "khenchela_en", {
   insets: [
     {
-      width: 500,
+      width: 1100,
       top: 0,
-      height: 420,
+      height: 1100,
       bbox: [{ y: 0, x: 300 }, { y: 0, x: 0 }],
       left: 0
     }
@@ -11,10 +11,90 @@ jQuery.fn.vectorMap("addMap", "khenchela_en", {
   paths: {
     "1": {
       path: "",
-      name: ""
+      name: "Khenchela"
+    },
+    "2": {
+      path: "",
+      name: "Mtoussa"
+    },
+    "3": {
+      path: "",
+      name: "Kais"
+    },
+    "4": {
+      path: "",
+      name: "Baghai"
+    },
+    "5": {
+      path: "",
+      name: "El Hamma"
+    },
+    "6": {
+      path: "",
+      name: "Ain Touila"
+    },
+    "7": {
+      path: "",
+      name: "Taouzianat"
+    },
+    "8": {
+      path: "",
+      name: "Bouhmama"
+    },
+    "9": {
+      path: "",
+      name: "El Oueldja"
+    },
+    "10": {
+      path: "",
+      name: "Remila"
+    },
+    "11": {
+      path: "",
+      name: "Cherchar"
+    },
+    "12": {
+      path: "",
+      name: "Djellal"
+    },
+    "13": {
+      path: "",
+      name: "Babar"
+    },
+    "14": {
+      path: "",
+      name: "Tamza"
+    },
+    "15": {
+      path: "",
+      name: "Ensigha"
+    },
+    "16": {
+      path: "",
+      name: "Ouled Rechache"
+    },
+    "17": {
+      path: "",
+      name: "El Mahmal"
+    },
+    "18": {
+      path: "",
+      name: "Msara"
+    },
+    "19": {
+      path: "",
+      name: "Yabous"
+    },
+    "20": {
+      path: "",
+      name: "Khirane"
+    },
+    "21": {
+      path: "",
+      name: "Chelia"
     }
   },
-  height: 420,
+  height: 1100,
   projection: { type: "mill", centralMeridian: 0.0 },
-  width: 500
+  width: 1100
 });
