d427fbc6b63326984b77e0a99987d0ad0ef221e2
Mohamed Ben <mohamed@ipfinder.io>
2019-09-02 22:52:33 +0100

    Add Communes de la wilaya el_oued

--- src/Wilaya/cities/en/el_oued_en.js
+++ src/Wilaya/cities/en/el_oued_en.js
@@ -1,9 +1,9 @@
 jQuery.fn.vectorMap("addMap", "el_oued_en", {
   insets: [
     {
-      width: 500,
+      width: 1100,
       top: 0,
-      height: 420,
+      height: 1100,
       bbox: [{ y: 0, x: 300 }, { y: 0, x: 0 }],
       left: 0
     }
@@ -11,10 +11,126 @@ jQuery.fn.vectorMap("addMap", "el_oued_en", {
   paths: {
     "1": {
       path: "",
-      name: ""
+      name: "El Oued"
+    },
+    "2": {
+      path: "",
+      name: "Robbah"
+    },
+    "3": {
+      path: "",
+      name: "Oued El Alenda"
+    },
+    "4": {
+      path: "",
+      name: "Bayadha"
+    },
+    "5": {
+      path: "",
+      name: "Nakhla"
+    },
+    "6": {
+      path: "",
+      name: "Guemar"
+    },
+    "7": {
+      path: "",
+      name: "Kouinine"
+    },
+    "8": {
+      path: "",
+      name: "Reguiba"
+    },
+    "9": {
+      path: "",
+      name: "Hamraia"
+    },
+    "10": {
+      path: "",
+      name: "Taghzout"
+    },
+    "11": {
+      path: "",
+      name: "Debila"
+    },
+    "12": {
+      path: "",
+      name: "Hassani Abdelkrim"
+    },
+    "13": {
+      path: "",
+      name: "Hassi Khelifa"
+    },
+    "14": {
+      path: "",
+      name: "Taleb Larbi"
+    },
+    "15": {
+      path: "",
+      name: "Douar El Ma"
+    },
+    "16": {
+      path: "",
+      name: "Sidi Aoun"
+    },
+    "17": {
+      path: "",
+      name: "Trifaoui"
+    },
+    "18": {
+      path: "",
+      name: "Magrane"
+    },
+    "19": {
+      path: "",
+      name: "Beni Guecha"
+    },
+    "20": {
+      path: "",
+      name: "Ourmas"
+    },
+    "21": {
+      path: "",
+      name: "Still"
+    },
+    "22": {
+      path: "",
+      name: "Mrara"
+    },
+    "23": {
+      path: "",
+      name: "Sidi Khellil"
+    },
+    "24": {
+      path: "",
+      name: "Tendla"
+    },
+    "25": {
+      path: "",
+      name: "El Ogla"
+    },
+    "26": {
+      path: "",
+      name: "Mih Ouansa"
+    },
+    "27": {
+      path: "",
+      name: "El Mghair"
+    },
+    "28": {
+      path: "",
+      name: "Djamaa"
+    },
+    "29": {
+      path: "",
+      name: "Oum Touyour"
+    },
+    "30": {
+      path: "",
+      name: "Sidi Amrane"
     }
   },
-  height: 420,
+  height: 1100,
   projection: { type: "mill", centralMeridian: 0.0 },
-  width: 500
+  width: 1100
 });
