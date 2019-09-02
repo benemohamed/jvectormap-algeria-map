4effd1ae3a85ec59b2a422417f9a7a13c0190fdc
Mohamed Ben <mohamed@ipfinder.io>
2019-09-02 22:52:32 +0100

    Add Communes de la wilaya jijel

--- src/Wilaya/cities/en/jijel_en.js
+++ src/Wilaya/cities/en/jijel_en.js
@@ -1,9 +1,9 @@
 jQuery.fn.vectorMap("addMap", "jijel_en", {
   insets: [
     {
-      width: 500,
+      width: 1100,
       top: 0,
-      height: 420,
+      height: 1100,
       bbox: [{ y: 0, x: 300 }, { y: 0, x: 0 }],
       left: 0
     }
@@ -11,10 +11,118 @@ jQuery.fn.vectorMap("addMap", "jijel_en", {
   paths: {
     "1": {
       path: "",
-      name: ""
+      name: "Jijel"
+    },
+    "2": {
+      path: "",
+      name: "Erraguene"
+    },
+    "3": {
+      path: "",
+      name: "El Aouana"
+    },
+    "4": {
+      path: "",
+      name: "Ziamma Mansouriah"
+    },
+    "5": {
+      path: "",
+      name: "Taher"
+    },
+    "6": {
+      path: "",
+      name: "Emir Abdelkader"
+    },
+    "7": {
+      path: "",
+      name: "Chekfa"
+    },
+    "8": {
+      path: "",
+      name: "Chahna"
+    },
+    "9": {
+      path: "",
+      name: "El Milia"
+    },
+    "10": {
+      path: "",
+      name: "Sidi Maarouf"
+    },
+    "11": {
+      path: "",
+      name: "Settara"
+    },
+    "12": {
+      path: "",
+      name: "El Ancer"
+    },
+    "13": {
+      path: "",
+      name: "Sidi Abdelaziz"
+    },
+    "14": {
+      path: "",
+      name: "Kaous"
+    },
+    "15": {
+      path: "",
+      name: "Ghebala"
+    },
+    "16": {
+      path: "",
+      name: "Bouraoui Belhadef"
+    },
+    "17": {
+      path: "",
+      name: "Djmila"
+    },
+    "18": {
+      path: "",
+      name: "Selma Benziada"
+    },
+    "19": {
+      path: "",
+      name: "Boussif Ouled Askeur"
+    },
+    "20": {
+      path: "",
+      name: "El Kennar Nouchfi"
+    },
+    "21": {
+      path: "",
+      name: "Ouled Yahia Khadrouch"
+    },
+    "22": {
+      path: "",
+      name: "Boudria Beni Yadjis"
+    },
+    "23": {
+      path: "",
+      name: "Kemir Oued Adjoul"
+    },
+    "24": {
+      path: "",
+      name: "Texena"
+    },
+    "25": {
+      path: "",
+      name: "Djemaa Beni Habibi"
+    },
+    "26": {
+      path: "",
+      name: "Bordj Taher"
+    },
+    "27": {
+      path: "",
+      name: "Ouled Rabah"
+    },
+    "28": {
+      path: "",
+      name: "Ouadjana"
     }
   },
-  height: 420,
+  height: 1100,
   projection: { type: "mill", centralMeridian: 0.0 },
-  width: 500
+  width: 1100
 });
