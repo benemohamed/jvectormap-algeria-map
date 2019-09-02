da7f38af6e6e3087100aedf08f7979ea94c7816c
Mohamed Ben <mohamed@ipfinder.io>
2019-09-02 22:52:33 +0100

    Add Communes de la wilaya mila

--- src/Wilaya/cities/en/mila_en.js
+++ src/Wilaya/cities/en/mila_en.js
@@ -1,9 +1,9 @@
 jQuery.fn.vectorMap("addMap", "mila_en", {
   insets: [
     {
-      width: 500,
+      width: 1100,
       top: 0,
-      height: 420,
+      height: 1100,
       bbox: [{ y: 0, x: 300 }, { y: 0, x: 0 }],
       left: 0
     }
@@ -11,10 +11,134 @@ jQuery.fn.vectorMap("addMap", "mila_en", {
   paths: {
     "1": {
       path: "",
-      name: ""
+      name: "Mila"
+    },
+    "2": {
+      path: "",
+      name: "Ferdjioua"
+    },
+    "3": {
+      path: "",
+      name: "Chelghoum Laid"
+    },
+    "4": {
+      path: "",
+      name: "Oued Athmenia"
+    },
+    "5": {
+      path: "",
+      name: "Ain Mellouk"
+    },
+    "6": {
+      path: "",
+      name: "Telerghma"
+    },
+    "7": {
+      path: "",
+      name: "Oued Seguen"
+    },
+    "8": {
+      path: "",
+      name: "Tadjenanet"
+    },
+    "9": {
+      path: "",
+      name: "Benyahia Abderrahmane"
+    },
+    "10": {
+      path: "",
+      name: "Oued Endja"
+    },
+    "11": {
+      path: "",
+      name: "Ahmed Rachedi"
+    },
+    "12": {
+      path: "",
+      name: "Ouled Khalouf"
+    },
+    "13": {
+      path: "",
+      name: "Tiberguent"
+    },
+    "14": {
+      path: "",
+      name: "Bouhatem"
+    },
+    "15": {
+      path: "",
+      name: "Rouached"
+    },
+    "16": {
+      path: "",
+      name: "Tessala Lamatai"
+    },
+    "17": {
+      path: "",
+      name: "Grarem Gouga"
+    },
+    "18": {
+      path: "",
+      name: "Sidi Merouane"
+    },
+    "19": {
+      path: "",
+      name: "Tassadane Haddada"
+    },
+    "20": {
+      path: "",
+      name: "Derradji Bousselah"
+    },
+    "21": {
+      path: "",
+      name: "Minar Zarza"
+    },
+    "22": {
+      path: "",
+      name: "Amira Arras"
+    },
+    "23": {
+      path: "",
+      name: "Terrai Bainen"
+    },
+    "24": {
+      path: "",
+      name: "Hamala"
+    },
+    "25": {
+      path: "",
+      name: "Ain Tine"
+    },
+    "26": {
+      path: "",
+      name: "El Mechira"
+    },
+    "27": {
+      path: "",
+      name: "Sidi Khelifa"
+    },
+    "28": {
+      path: "",
+      name: "Zeghaia"
+    },
+    "29": {
+      path: "",
+      name: "Elayadi Barbes"
+    },
+    "30": {
+      path: "",
+      name: "Ain Beida Harriche"
+    },
+    "31": {
+      path: "",
+      name: "Yahia Beniguecha"
+    },
+    "32": {
+      path: "",
+      name: "Chigara"
     }
   },
-  height: 420,
+  height: 1100,
   projection: { type: "mill", centralMeridian: 0.0 },
-  width: 500
+  width: 1100
 });
