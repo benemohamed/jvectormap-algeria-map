60e3464fbdcdf1427a7e8415aadbdbc29f0488ee
Mohamed Ben <mohamed@ipfinder.io>
2019-09-02 22:52:33 +0100

    Add Communes de la wilaya ain_temouchent

--- src/Wilaya/cities/en/ain_temouchent_en.js
+++ src/Wilaya/cities/en/ain_temouchent_en.js
@@ -1,9 +1,9 @@
 jQuery.fn.vectorMap("addMap", "ain_temouchent_en", {
   insets: [
     {
-      width: 500,
+      width: 1100,
       top: 0,
-      height: 420,
+      height: 1100,
       bbox: [{ y: 0, x: 300 }, { y: 0, x: 0 }],
       left: 0
     }
@@ -11,10 +11,118 @@ jQuery.fn.vectorMap("addMap", "ain_temouchent_en", {
   paths: {
     "1": {
       path: "",
-      name: ""
+      name: "Ain Temouchent"
+    },
+    "2": {
+      path: "",
+      name: "Chaabet El Ham"
+    },
+    "3": {
+      path: "",
+      name: "Ain Kihal"
+    },
+    "4": {
+      path: "",
+      name: "Hammam Bouhadjar"
+    },
+    "5": {
+      path: "",
+      name: "Bou Zedjar"
+    },
+    "6": {
+      path: "",
+      name: "Oued Berkeche"
+    },
+    "7": {
+      path: "",
+      name: "Aghlal"
+    },
+    "8": {
+      path: "",
+      name: "Terga"
+    },
+    "9": {
+      path: "",
+      name: "Ain El Arbaa"
+    },
+    "10": {
+      path: "",
+      name: "Tamzoura"
+    },
+    "11": {
+      path: "",
+      name: "Chentouf"
+    },
+    "12": {
+      path: "",
+      name: "Sidi Ben Adda"
+    },
+    "13": {
+      path: "",
+      name: "Aoubellil"
+    },
+    "14": {
+      path: "",
+      name: "El Malah"
+    },
+    "15": {
+      path: "",
+      name: "Sidi Boumediene"
+    },
+    "16": {
+      path: "",
+      name: "Oued Sabah"
+    },
+    "17": {
+      path: "",
+      name: "Ouled Boudjemaa"
+    },
+    "18": {
+      path: "",
+      name: "Ain Tolba"
+    },
+    "19": {
+      path: "",
+      name: "El Amria"
+    },
+    "20": {
+      path: "",
+      name: "Hassi El Ghella"
+    },
+    "21": {
+      path: "",
+      name: "Hassasna"
+    },
+    "22": {
+      path: "",
+      name: "Ouled Kihal"
+    },
+    "23": {
+      path: "",
+      name: "Beni Saf"
+    },
+    "24": {
+      path: "",
+      name: "Sidi Safi"
+    },
+    "25": {
+      path: "",
+      name: "Oulhaca El Gheraba"
+    },
+    "26": {
+      path: "",
+      name: "Tadmaya"
+    },
+    "27": {
+      path: "",
+      name: "El Emir Abdelkader"
+    },
+    "28": {
+      path: "",
+      name: "El Messaid"
     }
   },
-  height: 420,
+  height: 1100,
   projection: { type: "mill", centralMeridian: 0.0 },
-  width: 500
+  width: 1100
 });
