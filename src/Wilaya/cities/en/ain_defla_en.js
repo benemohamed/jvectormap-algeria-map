f8540ed0f72a76bc91f8ae0f1568c8652dd1d6ba
Mohamed Ben <mohamed@ipfinder.io>
2019-09-02 22:52:33 +0100

    Add Communes de la wilaya ain_defla

--- src/Wilaya/cities/en/ain_defla_en.js
+++ src/Wilaya/cities/en/ain_defla_en.js
@@ -1,9 +1,9 @@
 jQuery.fn.vectorMap("addMap", "ain_defla_en", {
   insets: [
     {
-      width: 500,
+      width: 1100,
       top: 0,
-      height: 420,
+      height: 1100,
       bbox: [{ y: 0, x: 300 }, { y: 0, x: 0 }],
       left: 0
     }
@@ -11,10 +11,150 @@ jQuery.fn.vectorMap("addMap", "ain_defla_en", {
   paths: {
     "1": {
       path: "",
-      name: ""
+      name: "Ain Defla"
+    },
+    "2": {
+      path: "",
+      name: "Miliana"
+    },
+    "3": {
+      path: "",
+      name: "Boumedfaa"
+    },
+    "4": {
+      path: "",
+      name: "Khemis Miliana"
+    },
+    "5": {
+      path: "",
+      name: "Hammam Righa"
+    },
+    "6": {
+      path: "",
+      name: "Arib"
+    },
+    "7": {
+      path: "",
+      name: "Djelida"
+    },
+    "8": {
+      path: "",
+      name: "El Amra"
+    },
+    "9": {
+      path: "",
+      name: "Bourached"
+    },
+    "10": {
+      path: "",
+      name: "El Attaf"
+    },
+    "11": {
+      path: "",
+      name: "El Abadia"
+    },
+    "12": {
+      path: "",
+      name: "Djendel"
+    },
+    "13": {
+      path: "",
+      name: "Oued Chorfa"
+    },
+    "14": {
+      path: "",
+      name: "Ain Lechiakh"
+    },
+    "15": {
+      path: "",
+      name: "Oued Djemaa"
+    },
+    "16": {
+      path: "",
+      name: "Rouina"
+    },
+    "17": {
+      path: "",
+      name: "Zeddine"
+    },
+    "18": {
+      path: "",
+      name: "El Hassania"
+    },
+    "19": {
+      path: "",
+      name: "Bir Ouled Khelifa"
+    },
+    "20": {
+      path: "",
+      name: "Ain Soltane"
+    },
+    "21": {
+      path: "",
+      name: "Tarik Ibn Ziad"
+    },
+    "22": {
+      path: "",
+      name: "Bordj Emir Khaled"
+    },
+    "23": {
+      path: "",
+      name: "Ain Torki"
+    },
+    "24": {
+      path: "",
+      name: "Sidi Lakhdar"
+    },
+    "25": {
+      path: "",
+      name: "Ben Allal"
+    },
+    "26": {
+      path: "",
+      name: "Ain Benian"
+    },
+    "27": {
+      path: "",
+      name: "Hoceinia"
+    },
+    "28": {
+      path: "",
+      name: "Barbouche"
+    },
+    "29": {
+      path: "",
+      name: "Djemaa Ouled Chikh"
+    },
+    "30": {
+      path: "",
+      name: "Mekhatria"
+    },
+    "31": {
+      path: "",
+      name: "Bathia"
+    },
+    "32": {
+      path: "",
+      name: "Tachta Zegagha"
+    },
+    "33": {
+      path: "",
+      name: "Ain Bouyahia"
+    },
+    "34": {
+      path: "",
+      name: "El Maine"
+    },
+    "35": {
+      path: "",
+      name: "Tiberkanine"
+    },
+    "36": {
+      path: "",
+      name: "Belaas"
     }
   },
-  height: 420,
+  height: 1100,
   projection: { type: "mill", centralMeridian: 0.0 },
-  width: 500
+  width: 1100
 });
