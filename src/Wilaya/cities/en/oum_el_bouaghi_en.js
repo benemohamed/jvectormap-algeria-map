a4e130350b0af1981adb4be8cca66387c8b622ab
Mohamed Ben <mohamed@ipfinder.io>
2019-09-02 22:52:31 +0100

    Add Communes de la wilaya oum_el_bouaghi

--- src/Wilaya/cities/en/oum_el_bouaghi_en.js
+++ src/Wilaya/cities/en/oum_el_bouaghi_en.js
@@ -1,9 +1,9 @@
 jQuery.fn.vectorMap("addMap", "oum_el_bouaghi_en", {
   insets: [
     {
-      width: 500,
+      width: 1100,
       top: 0,
-      height: 420,
+      height: 1100,
       bbox: [{ y: 0, x: 300 }, { y: 0, x: 0 }],
       left: 0
     }
@@ -11,10 +11,122 @@ jQuery.fn.vectorMap("addMap", "oum_el_bouaghi_en", {
   paths: {
     "1": {
       path: "",
-      name: ""
+      name: "Oum El Bouaghi"
+    },
+    "2": {
+      path: "",
+      name: "Ain Beida"
+    },
+    "3": {
+      path: "",
+      name: "Ainmlila"
+    },
+    "4": {
+      path: "",
+      name: "Behir Chergui"
+    },
+    "5": {
+      path: "",
+      name: "El Amiria"
+    },
+    "6": {
+      path: "",
+      name: "Sigus"
+    },
+    "7": {
+      path: "",
+      name: "El Belala"
+    },
+    "8": {
+      path: "",
+      name: "Ain Babouche"
+    },
+    "9": {
+      path: "",
+      name: "Berriche"
+    },
+    "10": {
+      path: "",
+      name: "Ouled Hamla"
+    },
+    "11": {
+      path: "",
+      name: "Dhala"
+    },
+    "12": {
+      path: "",
+      name: "Ain Kercha"
+    },
+    "13": {
+      path: "",
+      name: "Hanchir Toumghani"
+    },
+    "14": {
+      path: "",
+      name: "El Djazia"
+    },
+    "15": {
+      path: "",
+      name: "Ain Diss"
+    },
+    "16": {
+      path: "",
+      name: "Fkirina"
+    },
+    "17": {
+      path: "",
+      name: "Souk Naamane"
+    },
+    "18": {
+      path: "",
+      name: "Zorg"
+    },
+    "19": {
+      path: "",
+      name: "El Fedjoudj Boughrar"
+    },
+    "20": {
+      path: "",
+      name: "Ouled Zouai"
+    },
+    "21": {
+      path: "",
+      name: "Bir Chouhada"
+    },
+    "22": {
+      path: "",
+      name: "Ksar Sbahi"
+    },
+    "23": {
+      path: "",
+      name: "Oued Nini"
+    },
+    "24": {
+      path: "",
+      name: "Meskiana"
+    },
+    "25": {
+      path: "",
+      name: "Ain Fekroune"
+    },
+    "26": {
+      path: "",
+      name: "Rahia"
+    },
+    "27": {
+      path: "",
+      name: "Ain Zitoun"
+    },
+    "28": {
+      path: "",
+      name: "Ouled Gacem"
+    },
+    "29": {
+      path: "",
+      name: "El Harmilia"
     }
   },
-  height: 420,
+  height: 1100,
   projection: { type: "mill", centralMeridian: 0.0 },
-  width: 500
+  width: 1100
 });
