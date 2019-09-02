fd1a6dc5f06a5ce2b1f91a22cecdf8b9f97e3594
Mohamed Ben <mohamed@ipfinder.io>
2019-09-02 22:52:32 +0100

    Add Communes de la wilaya ouargla

--- src/Wilaya/cities/en/ouargla_en.js
+++ src/Wilaya/cities/en/ouargla_en.js
@@ -1,9 +1,9 @@
 jQuery.fn.vectorMap("addMap", "ouargla_en", {
   insets: [
     {
-      width: 500,
+      width: 1100,
       top: 0,
-      height: 420,
+      height: 1100,
       bbox: [{ y: 0, x: 300 }, { y: 0, x: 0 }],
       left: 0
     }
@@ -11,10 +11,90 @@ jQuery.fn.vectorMap("addMap", "ouargla_en", {
   paths: {
     "1": {
       path: "",
-      name: ""
+      name: "Ouargla"
+    },
+    "2": {
+      path: "",
+      name: "Ain Beida"
+    },
+    "3": {
+      path: "",
+      name: "Ngoussa"
+    },
+    "4": {
+      path: "",
+      name: "Hassi Messaoud"
+    },
+    "5": {
+      path: "",
+      name: "Rouissat"
+    },
+    "6": {
+      path: "",
+      name: "Balidat Ameur"
+    },
+    "7": {
+      path: "",
+      name: "Tebesbest"
+    },
+    "8": {
+      path: "",
+      name: "Nezla"
+    },
+    "9": {
+      path: "",
+      name: "Zaouia El Abidia"
+    },
+    "10": {
+      path: "",
+      name: "Sidi Slimane"
+    },
+    "11": {
+      path: "",
+      name: "Sidi Khouiled"
+    },
+    "12": {
+      path: "",
+      name: "Hassi Ben Abdellah"
+    },
+    "13": {
+      path: "",
+      name: "Touggourt"
+    },
+    "14": {
+      path: "",
+      name: "El Hadjira"
+    },
+    "15": {
+      path: "",
+      name: "Taibet"
+    },
+    "16": {
+      path: "",
+      name: "Tamacine"
+    },
+    "17": {
+      path: "",
+      name: "Benaceur"
+    },
+    "18": {
+      path: "",
+      name: "Mnaguer"
+    },
+    "19": {
+      path: "",
+      name: "Megarine"
+    },
+    "20": {
+      path: "",
+      name: "El Allia"
+    },
+    "21": {
+      path: "",
+      name: "El Borma"
     }
   },
-  height: 420,
+  height: 1100,
   projection: { type: "mill", centralMeridian: 0.0 },
-  width: 500
+  width: 1100
 });
