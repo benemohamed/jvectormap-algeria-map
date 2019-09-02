fc4f507f0b230f8e24075c7932e9b6b07479c122
Mohamed Ben <mohamed@ipfinder.io>
2019-09-02 22:52:32 +0100

    Add Communes de la wilaya bouira

--- src/Wilaya/cities/en/bouira_en.js
+++ src/Wilaya/cities/en/bouira_en.js
@@ -1,9 +1,9 @@
 jQuery.fn.vectorMap("addMap", "bouira_en", {
   insets: [
     {
-      width: 500,
+      width: 1100,
       top: 0,
-      height: 420,
+      height: 1100,
       bbox: [{ y: 0, x: 300 }, { y: 0, x: 0 }],
       left: 0
     }
@@ -11,10 +11,186 @@ jQuery.fn.vectorMap("addMap", "bouira_en", {
   paths: {
     "1": {
       path: "",
-      name: ""
+      name: "Bouira"
+    },
+    "2": {
+      path: "",
+      name: "El Asnam"
+    },
+    "3": {
+      path: "",
+      name: "Guerrouma"
+    },
+    "4": {
+      path: "",
+      name: "Souk El Khemis"
+    },
+    "5": {
+      path: "",
+      name: "Kadiria"
+    },
+    "6": {
+      path: "",
+      name: "Hanif"
+    },
+    "7": {
+      path: "",
+      name: "Dirah"
+    },
+    "8": {
+      path: "",
+      name: "Ait Laaziz"
+    },
+    "9": {
+      path: "",
+      name: "Taghzout"
+    },
+    "10": {
+      path: "",
+      name: "Raouraoua"
+    },
+    "11": {
+      path: "",
+      name: "Mezdour"
+    },
+    "12": {
+      path: "",
+      name: "Haizer"
+    },
+    "13": {
+      path: "",
+      name: "Lakhdaria"
+    },
+    "14": {
+      path: "",
+      name: "Maala"
+    },
+    "15": {
+      path: "",
+      name: "El Hachimia"
+    },
+    "16": {
+      path: "",
+      name: "Aomar"
+    },
+    "17": {
+      path: "",
+      name: "Chorfa"
+    },
+    "18": {
+      path: "",
+      name: "Bordj Oukhriss"
+    },
+    "19": {
+      path: "",
+      name: "El Adjiba"
+    },
+    "20": {
+      path: "",
+      name: "El Hakimia"
+    },
+    "21": {
+      path: "",
+      name: "El Khebouzia"
+    },
+    "22": {
+      path: "",
+      name: "Ahl El Ksar"
+    },
+    "23": {
+      path: "",
+      name: "Bouderbala"
+    },
+    "24": {
+      path: "",
+      name: "Zbarbar"
+    },
+    "25": {
+      path: "",
+      name: "Ain El Hadjar"
+    },
+    "26": {
+      path: "",
+      name: "Djebahia"
+    },
+    "27": {
+      path: "",
+      name: "Aghbalou"
+    },
+    "28": {
+      path: "",
+      name: "Taguedit"
+    },
+    "29": {
+      path: "",
+      name: "Ain Turk"
+    },
+    "30": {
+      path: "",
+      name: "Saharidj"
+    },
+    "31": {
+      path: "",
+      name: "Dechmia"
+    },
+    "32": {
+      path: "",
+      name: "Ridane"
+    },
+    "33": {
+      path: "",
+      name: "Bechloul"
+    },
+    "34": {
+      path: "",
+      name: "Boukram"
+    },
+    "35": {
+      path: "",
+      name: "Ain Bessam"
+    },
+    "36": {
+      path: "",
+      name: "Bir Ghbalou"
+    },
+    "37": {
+      path: "",
+      name: "Mchedallah"
+    },
+    "38": {
+      path: "",
+      name: "Sour El Ghozlane"
+    },
+    "39": {
+      path: "",
+      name: "Maamora"
+    },
+    "40": {
+      path: "",
+      name: "Ouled Rached"
+    },
+    "41": {
+      path: "",
+      name: "Ain Laloui"
+    },
+    "42": {
+      path: "",
+      name: "Hadjera Zerga"
+    },
+    "43": {
+      path: "",
+      name: "Ath Mansour"
+    },
+    "44": {
+      path: "",
+      name: "El Mokrani"
+    },
+    "45": {
+      path: "",
+      name: "Oued El Berdi"
     }
   },
-  height: 420,
+  height: 1100,
   projection: { type: "mill", centralMeridian: 0.0 },
-  width: 500
+  width: 1100
 });
