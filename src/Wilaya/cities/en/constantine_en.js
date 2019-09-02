914dd027da9e4f9f7c0b24c554118cb4326b145a
Mohamed Ben <mohamed@ipfinder.io>
2019-09-02 22:52:32 +0100

    Add Communes de la wilaya constantine

--- src/Wilaya/cities/en/constantine_en.js
+++ src/Wilaya/cities/en/constantine_en.js
@@ -1,9 +1,9 @@
 jQuery.fn.vectorMap("addMap", "constantine_en", {
   insets: [
     {
-      width: 500,
+      width: 1100,
       top: 0,
-      height: 420,
+      height: 1100,
       bbox: [{ y: 0, x: 300 }, { y: 0, x: 0 }],
       left: 0
     }
@@ -11,10 +11,54 @@ jQuery.fn.vectorMap("addMap", "constantine_en", {
   paths: {
     "1": {
       path: "",
-      name: ""
+      name: "Constantine"
+    },
+    "2": {
+      path: "",
+      name: "Hamma Bouziane"
+    },
+    "3": {
+      path: "",
+      name: "El Haria"
+    },
+    "4": {
+      path: "",
+      name: "Zighoud Youcef"
+    },
+    "5": {
+      path: "",
+      name: "Didouche Mourad"
+    },
+    "6": {
+      path: "",
+      name: "El Khroub"
+    },
+    "7": {
+      path: "",
+      name: "Ain Abid"
+    },
+    "8": {
+      path: "",
+      name: "Beni Hamiden"
+    },
+    "9": {
+      path: "",
+      name: "Ouled Rahmoune"
+    },
+    "10": {
+      path: "",
+      name: "Ain Smara"
+    },
+    "11": {
+      path: "",
+      name: "Mesaoud Boudjeriou"
+    },
+    "12": {
+      path: "",
+      name: "Ibn Ziad"
     }
   },
-  height: 420,
+  height: 1100,
   projection: { type: "mill", centralMeridian: 0.0 },
-  width: 500
+  width: 1100
 });
