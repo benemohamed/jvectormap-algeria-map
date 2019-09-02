c92a27430c95762ccd683a261bb5d08b69f97ac6
Mohamed Ben <mohamed@ipfinder.io>
2019-09-02 22:52:31 +0100

    Add Communes de la wilaya batna

--- src/Wilaya/cities/en/batna_en.js
+++ src/Wilaya/cities/en/batna_en.js
@@ -1,9 +1,9 @@
 jQuery.fn.vectorMap("addMap", "batna_en", {
   insets: [
     {
-      width: 500,
+      width: 1100,
       top: 0,
-      height: 420,
+      height: 1100,
       bbox: [{ y: 0, x: 300 }, { y: 0, x: 0 }],
       left: 0
     }
@@ -11,10 +11,250 @@ jQuery.fn.vectorMap("addMap", "batna_en", {
   paths: {
     "1": {
       path: "",
-      name: ""
+      name: "Batna"
+    },
+    "2": {
+      path: "",
+      name: "Ghassira"
+    },
+    "3": {
+      path: "",
+      name: "Maafa"
+    },
+    "4": {
+      path: "",
+      name: "Merouana"
+    },
+    "5": {
+      path: "",
+      name: "Seriana"
+    },
+    "6": {
+      path: "",
+      name: "Menaa"
+    },
+    "7": {
+      path: "",
+      name: "El Madher"
+    },
+    "8": {
+      path: "",
+      name: "Tazoult"
+    },
+    "9": {
+      path: "",
+      name: "Ngaous"
+    },
+    "10": {
+      path: "",
+      name: "Guigba"
+    },
+    "11": {
+      path: "",
+      name: "Inoughissen"
+    },
+    "12": {
+      path: "",
+      name: "Ouyoun El Assafir"
+    },
+    "13": {
+      path: "",
+      name: "Djerma"
+    },
+    "14": {
+      path: "",
+      name: "Bitam"
+    },
+    "15": {
+      path: "",
+      name: "Metkaouak"
+    },
+    "16": {
+      path: "",
+      name: "Arris"
+    },
+    "17": {
+      path: "",
+      name: "Kimmel"
+    },
+    "18": {
+      path: "",
+      name: "Tilatou"
+    },
+    "19": {
+      path: "",
+      name: "Ain Djasser"
+    },
+    "20": {
+      path: "",
+      name: "Ouled Selam"
+    },
+    "21": {
+      path: "",
+      name: "Tigherghar"
+    },
+    "22": {
+      path: "",
+      name: "Ain Yagout"
+    },
+    "23": {
+      path: "",
+      name: "Fesdis"
+    },
+    "24": {
+      path: "",
+      name: "Sefiane"
+    },
+    "25": {
+      path: "",
+      name: "Rahbat"
+    },
+    "26": {
+      path: "",
+      name: "Tighanimine"
+    },
+    "27": {
+      path: "",
+      name: "Lemsane"
+    },
+    "28": {
+      path: "",
+      name: "Ksar Belezma"
+    },
+    "29": {
+      path: "",
+      name: "Seggana"
+    },
+    "30": {
+      path: "",
+      name: "Ichmoul"
+    },
+    "31": {
+      path: "",
+      name: "Foum Toub"
+    },
+    "32": {
+      path: "",
+      name: "Beni Foudhala El Hakania"
+    },
+    "33": {
+      path: "",
+      name: "Oued El Ma"
+    },
+    "34": {
+      path: "",
+      name: "Talkhamt"
+    },
+    "35": {
+      path: "",
+      name: "Bouzina"
+    },
+    "36": {
+      path: "",
+      name: "Chemora"
+    },
+    "37": {
+      path: "",
+      name: "Oued Chaaba"
+    },
+    "38": {
+      path: "",
+      name: "Taxlent"
+    },
+    "39": {
+      path: "",
+      name: "Gosbat"
+    },
+    "40": {
+      path: "",
+      name: "Ouled Aouf"
+    },
+    "41": {
+      path: "",
+      name: "Boumagueur"
+    },
+    "42": {
+      path: "",
+      name: "Barika"
+    },
+    "43": {
+      path: "",
+      name: "Djezzar"
+    },
+    "44": {
+      path: "",
+      name: "Tkout"
+    },
+    "45": {
+      path: "",
+      name: "Ain Touta"
+    },
+    "46": {
+      path: "",
+      name: "Hidoussa"
+    },
+    "47": {
+      path: "",
+      name: "Teniet El Abed"
+    },
+    "48": {
+      path: "",
+      name: "Oued Taga"
+    },
+    "49": {
+      path: "",
+      name: "Ouled Fadel"
+    },
+    "50": {
+      path: "",
+      name: "Timgad"
+    },
+    "51": {
+      path: "",
+      name: "Ras El Aioun"
+    },
+    "52": {
+      path: "",
+      name: "Chir"
+    },
+    "53": {
+      path: "",
+      name: "Ouled Si Slimane"
+    },
+    "54": {
+      path: "",
+      name: "Zanat El Beida"
+    },
+    "55": {
+      path: "",
+      name: "Amdoukal"
+    },
+    "56": {
+      path: "",
+      name: "Ouled Ammar"
+    },
+    "57": {
+      path: "",
+      name: "El Hassi"
+    },
+    "58": {
+      path: "",
+      name: "Lazrou"
+    },
+    "59": {
+      path: "",
+      name: "Boumia"
+    },
+    "60": {
+      path: "",
+      name: "Boulhilat"
+    },
+    "61": {
+      path: "",
+      name: "Larbaa"
     }
   },
-  height: 420,
+  height: 1100,
   projection: { type: "mill", centralMeridian: 0.0 },
-  width: 500
+  width: 1100
 });
