dae6c16844f4b35bb8879eb64cd50d3201e731e8
Mohamed Ben <mohamed@ipfinder.io>
2019-09-02 22:52:32 +0100

    Add Communes de la wilaya oran

--- src/Wilaya/cities/en/oran_en.js
+++ src/Wilaya/cities/en/oran_en.js
@@ -1,9 +1,9 @@
 jQuery.fn.vectorMap("addMap", "oran_en", {
   insets: [
     {
-      width: 500,
+      width: 1100,
       top: 0,
-      height: 420,
+      height: 1100,
       bbox: [{ y: 0, x: 300 }, { y: 0, x: 0 }],
       left: 0
     }
@@ -11,10 +11,110 @@ jQuery.fn.vectorMap("addMap", "oran_en", {
   paths: {
     "1": {
       path: "",
-      name: ""
+      name: "Oran"
+    },
+    "2": {
+      path: "",
+      name: "Gdyel"
+    },
+    "3": {
+      path: "",
+      name: "Bir El Djir"
+    },
+    "4": {
+      path: "",
+      name: "Hassi Bounif"
+    },
+    "5": {
+      path: "",
+      name: "Es Senia"
+    },
+    "6": {
+      path: "",
+      name: "Arzew"
+    },
+    "7": {
+      path: "",
+      name: "Bethioua"
+    },
+    "8": {
+      path: "",
+      name: "Marsat El Hadjadj"
+    },
+    "9": {
+      path: "",
+      name: "Ain Turk"
+    },
+    "10": {
+      path: "",
+      name: "El Ancar"
+    },
+    "11": {
+      path: "",
+      name: "Oued Tlelat"
+    },
+    "12": {
+      path: "",
+      name: "Tafraoui"
+    },
+    "13": {
+      path: "",
+      name: "Sidi Chami"
+    },
+    "14": {
+      path: "",
+      name: "Boufatis"
+    },
+    "15": {
+      path: "",
+      name: "Mers El Kebir"
+    },
+    "16": {
+      path: "",
+      name: "Bousfer"
+    },
+    "17": {
+      path: "",
+      name: "El Karma"
+    },
+    "18": {
+      path: "",
+      name: "El Braya"
+    },
+    "19": {
+      path: "",
+      name: "Hassi Ben Okba"
+    },
+    "20": {
+      path: "",
+      name: "Ben Freha"
+    },
+    "21": {
+      path: "",
+      name: "Hassi Mefsoukh"
+    },
+    "22": {
+      path: "",
+      name: "Sidi Ben Yabka"
+    },
+    "23": {
+      path: "",
+      name: "Messerghin"
+    },
+    "24": {
+      path: "",
+      name: "Boutlelis"
+    },
+    "25": {
+      path: "",
+      name: "Ain Kerma"
+    },
+    "26": {
+      path: "",
+      name: "Ain Biya"
     }
   },
-  height: 420,
+  height: 1100,
   projection: { type: "mill", centralMeridian: 0.0 },
-  width: 500
+  width: 1100
 });
