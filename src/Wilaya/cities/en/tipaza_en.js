f62f8d3eba667709370bf61b9c9abbefa8d9dbcb
Mohamed Ben <mohamed@ipfinder.io>
2019-09-02 22:52:33 +0100

    Add Communes de la wilaya tipaza

--- src/Wilaya/cities/en/tipaza_en.js
+++ src/Wilaya/cities/en/tipaza_en.js
@@ -1,9 +1,9 @@
 jQuery.fn.vectorMap("addMap", "tipaza_en", {
   insets: [
     {
-      width: 500,
+      width: 1100,
       top: 0,
-      height: 420,
+      height: 1100,
       bbox: [{ y: 0, x: 300 }, { y: 0, x: 0 }],
       left: 0
     }
@@ -11,10 +11,118 @@ jQuery.fn.vectorMap("addMap", "tipaza_en", {
   paths: {
     "1": {
       path: "",
-      name: ""
+      name: "Tipaza"
+    },
+    "2": {
+      path: "",
+      name: "Menaceur"
+    },
+    "3": {
+      path: "",
+      name: "Larhat"
+    },
+    "4": {
+      path: "",
+      name: "Douaouda"
+    },
+    "5": {
+      path: "",
+      name: "Bourkika"
+    },
+    "6": {
+      path: "",
+      name: "Khemisti"
+    },
+    "7": {
+      path: "",
+      name: "Aghabal"
+    },
+    "8": {
+      path: "",
+      name: "Hadjout"
+    },
+    "9": {
+      path: "",
+      name: "Sidi Amar"
+    },
+    "10": {
+      path: "",
+      name: "Gouraya"
+    },
+    "11": {
+      path: "",
+      name: "Nodor"
+    },
+    "12": {
+      path: "",
+      name: "Chaiba"
+    },
+    "13": {
+      path: "",
+      name: "Ain Tagourait"
+    },
+    "14": {
+      path: "",
+      name: "Cherchel"
+    },
+    "15": {
+      path: "",
+      name: "Damous"
+    },
+    "16": {
+      path: "",
+      name: "Meurad"
+    },
+    "17": {
+      path: "",
+      name: "Fouka"
+    },
+    "18": {
+      path: "",
+      name: "Bou Ismail"
+    },
+    "19": {
+      path: "",
+      name: "Ahmer El Ain"
+    },
+    "20": {
+      path: "",
+      name: "Bou Haroun"
+    },
+    "21": {
+      path: "",
+      name: "Sidi Ghiles"
+    },
+    "22": {
+      path: "",
+      name: "Messelmoun"
+    },
+    "23": {
+      path: "",
+      name: "Sidi Rached"
+    },
+    "24": {
+      path: "",
+      name: "Kolea"
+    },
+    "25": {
+      path: "",
+      name: "Attatba"
+    },
+    "26": {
+      path: "",
+      name: "Sidi Semiane"
+    },
+    "27": {
+      path: "",
+      name: "Beni Milleuk"
+    },
+    "28": {
+      path: "",
+      name: "Hadjerat Ennous"
     }
   },
-  height: 420,
+  height: 1100,
   projection: { type: "mill", centralMeridian: 0.0 },
-  width: 500
+  width: 1100
 });
