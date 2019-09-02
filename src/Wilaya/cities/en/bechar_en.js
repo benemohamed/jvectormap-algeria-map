1e311334f75965a456ebe0102966c5ca3185cf36
Mohamed Ben <mohamed@ipfinder.io>
2019-09-02 22:52:31 +0100

    Add Communes de la wilaya bechar

--- src/Wilaya/cities/en/bechar_en.js
+++ src/Wilaya/cities/en/bechar_en.js
@@ -1,9 +1,9 @@
 jQuery.fn.vectorMap("addMap", "bechar_en", {
   insets: [
     {
-      width: 500,
+      width: 1100,
       top: 0,
-      height: 420,
+      height: 1100,
       bbox: [{ y: 0, x: 300 }, { y: 0, x: 0 }],
       left: 0
     }
@@ -11,10 +11,90 @@ jQuery.fn.vectorMap("addMap", "bechar_en", {
   paths: {
     "1": {
       path: "",
-      name: ""
+      name: "Bechar"
+    },
+    "2": {
+      path: "",
+      name: "Erg Ferradj"
+    },
+    "3": {
+      path: "",
+      name: "Ouled Khoudir"
+    },
+    "4": {
+      path: "",
+      name: "Meridja"
+    },
+    "5": {
+      path: "",
+      name: "Timoudi"
+    },
+    "6": {
+      path: "",
+      name: "Lahmar"
+    },
+    "7": {
+      path: "",
+      name: "Beni Abbes"
+    },
+    "8": {
+      path: "",
+      name: "Beni Ikhlef"
+    },
+    "9": {
+      path: "",
+      name: "Mechraa Houari B"
+    },
+    "10": {
+      path: "",
+      name: "Kenedsa"
+    },
+    "11": {
+      path: "",
+      name: "Igli"
+    },
+    "12": {
+      path: "",
+      name: "Tabalbala"
+    },
+    "13": {
+      path: "",
+      name: "Taghit"
+    },
+    "14": {
+      path: "",
+      name: "El Ouata"
+    },
+    "15": {
+      path: "",
+      name: "Boukais"
+    },
+    "16": {
+      path: "",
+      name: "Mogheul"
+    },
+    "17": {
+      path: "",
+      name: "Abadla"
+    },
+    "18": {
+      path: "",
+      name: "Kerzaz"
+    },
+    "19": {
+      path: "",
+      name: "Ksabi"
+    },
+    "20": {
+      path: "",
+      name: "Tamtert"
+    },
+    "21": {
+      path: "",
+      name: "Beni Ounif"
     }
   },
-  height: 420,
+  height: 1100,
   projection: { type: "mill", centralMeridian: 0.0 },
-  width: 500
+  width: 1100
 });
