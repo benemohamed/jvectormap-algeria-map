3121d0e93392fb660b57ec25bbbb05326f3bdb4a
Mohamed Ben <mohamed@ipfinder.io>
2019-09-02 22:52:33 +0100

    Add Communes de la wilaya souk_ahras

--- src/Wilaya/cities/en/souk_ahras_en.js
+++ src/Wilaya/cities/en/souk_ahras_en.js
@@ -1,9 +1,9 @@
 jQuery.fn.vectorMap("addMap", "souk_ahras_en", {
   insets: [
     {
-      width: 500,
+      width: 1100,
       top: 0,
-      height: 420,
+      height: 1100,
       bbox: [{ y: 0, x: 300 }, { y: 0, x: 0 }],
       left: 0
     }
@@ -11,10 +11,110 @@ jQuery.fn.vectorMap("addMap", "souk_ahras_en", {
   paths: {
     "1": {
       path: "",
-      name: ""
+      name: "Souk Ahras"
+    },
+    "2": {
+      path: "",
+      name: "Sedrata"
+    },
+    "3": {
+      path: "",
+      name: "Hanancha"
+    },
+    "4": {
+      path: "",
+      name: "Mechroha"
+    },
+    "5": {
+      path: "",
+      name: "Ouled Driss"
+    },
+    "6": {
+      path: "",
+      name: "Tiffech"
+    },
+    "7": {
+      path: "",
+      name: "Zaarouria"
+    },
+    "8": {
+      path: "",
+      name: "Taoura"
+    },
+    "9": {
+      path: "",
+      name: "Drea"
+    },
+    "10": {
+      path: "",
+      name: "Haddada"
+    },
+    "11": {
+      path: "",
+      name: "Khedara"
+    },
+    "12": {
+      path: "",
+      name: "Merahna"
+    },
+    "13": {
+      path: "",
+      name: "Ouled Moumen"
+    },
+    "14": {
+      path: "",
+      name: "Bir Bouhouche"
+    },
+    "15": {
+      path: "",
+      name: "Mdaourouche"
+    },
+    "16": {
+      path: "",
+      name: "Oum El Adhaim"
+    },
+    "17": {
+      path: "",
+      name: "Ain Zana"
+    },
+    "18": {
+      path: "",
+      name: "Ain Soltane"
+    },
+    "19": {
+      path: "",
+      name: "Quillen"
+    },
+    "20": {
+      path: "",
+      name: "Sidi Fredj"
+    },
+    "21": {
+      path: "",
+      name: "Safel El Ouiden"
+    },
+    "22": {
+      path: "",
+      name: "Ragouba"
+    },
+    "23": {
+      path: "",
+      name: "Khemissa"
+    },
+    "24": {
+      path: "",
+      name: "Oued Keberit"
+    },
+    "25": {
+      path: "",
+      name: "Terraguelt"
+    },
+    "26": {
+      path: "",
+      name: "Zouabi"
     }
   },
-  height: 420,
+  height: 1100,
   projection: { type: "mill", centralMeridian: 0.0 },
-  width: 500
+  width: 1100
 });
