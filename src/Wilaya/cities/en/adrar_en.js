ee2d4f5c27d82f3cd681c02d6fd13b3a7d2a4c6f
Mohamed Ben <mohamed@ipfinder.io>
2019-09-02 22:52:23 +0100

    Add Communes de la wilaya adrar

--- src/Wilaya/cities/en/adrar_en.js
+++ src/Wilaya/cities/en/adrar_en.js
@@ -1,9 +1,9 @@
 jQuery.fn.vectorMap("addMap", "adrar_en", {
   insets: [
     {
-      width: 500,
+      width: 1100,
       top: 0,
-      height: 420,
+      height: 1100,
       bbox: [{ y: 0, x: 300 }, { y: 0, x: 0 }],
       left: 0
     }
@@ -11,10 +11,118 @@ jQuery.fn.vectorMap("addMap", "adrar_en", {
   paths: {
     "1": {
       path: "",
-      name: ""
+      name: "Adrar"
+    },
+    "2": {
+      path: "",
+      name: "Tamest"
+    },
+    "3": {
+      path: "",
+      name: "Charouine"
+    },
+    "4": {
+      path: "",
+      name: "Reggane"
+    },
+    "5": {
+      path: "",
+      name: "Inozghmir"
+    },
+    "6": {
+      path: "",
+      name: "Tit"
+    },
+    "7": {
+      path: "",
+      name: "Ksar Kaddour"
+    },
+    "8": {
+      path: "",
+      name: "Tsabit"
+    },
+    "9": {
+      path: "",
+      name: "Timimoun"
+    },
+    "10": {
+      path: "",
+      name: "Ouled Said"
+    },
+    "11": {
+      path: "",
+      name: "Zaouiet Kounta"
+    },
+    "12": {
+      path: "",
+      name: "Aoulef"
+    },
+    "13": {
+      path: "",
+      name: "Timokten"
+    },
+    "14": {
+      path: "",
+      name: "Tamentit"
+    },
+    "15": {
+      path: "",
+      name: "Fenoughil"
+    },
+    "16": {
+      path: "",
+      name: "Tinerkouk"
+    },
+    "17": {
+      path: "",
+      name: "Deldoul"
+    },
+    "18": {
+      path: "",
+      name: "Sali"
+    },
+    "19": {
+      path: "",
+      name: "Akabli"
+    },
+    "20": {
+      path: "",
+      name: "Metarfa"
+    },
+    "21": {
+      path: "",
+      name: "O Ahmed Timmi"
+    },
+    "22": {
+      path: "",
+      name: "Bouda"
+    },
+    "23": {
+      path: "",
+      name: "Aougrout"
+    },
+    "24": {
+      path: "",
+      name: "Talmine"
+    },
+    "25": {
+      path: "",
+      name: "B Badji Mokhtar"
+    },
+    "26": {
+      path: "",
+      name: "Sbaa"
+    },
+    "27": {
+      path: "",
+      name: "Ouled Aissa"
+    },
+    "28": {
+      path: "",
+      name: "Timiaouine"
     }
   },
-  height: 420,
+  height: 1100,
   projection: { type: "mill", centralMeridian: 0.0 },
-  width: 500
+  width: 1100
 });
