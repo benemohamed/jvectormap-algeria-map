b214466db885609898f383f46960d52d83bc7a94
Mohamed Ben <mohamed@ipfinder.io>
2019-09-02 22:52:33 +0100

    Add Communes de la wilaya illizi

--- src/Wilaya/cities/en/illizi_en.js
+++ src/Wilaya/cities/en/illizi_en.js
@@ -1,9 +1,9 @@
 jQuery.fn.vectorMap("addMap", "illizi_en", {
   insets: [
     {
-      width: 500,
+      width: 1100,
       top: 0,
-      height: 420,
+      height: 1100,
       bbox: [{ y: 0, x: 300 }, { y: 0, x: 0 }],
       left: 0
     }
@@ -11,10 +11,30 @@ jQuery.fn.vectorMap("addMap", "illizi_en", {
   paths: {
     "1": {
       path: "",
-      name: ""
+      name: "Illizi"
+    },
+    "2": {
+      path: "",
+      name: "Djanet"
+    },
+    "3": {
+      path: "",
+      name: "Debdeb"
+    },
+    "4": {
+      path: "",
+      name: "Bordj Omar Driss"
+    },
+    "5": {
+      path: "",
+      name: "Bordj El Haouasse"
+    },
+    "6": {
+      path: "",
+      name: "In Amenas"
     }
   },
-  height: 420,
+  height: 1100,
   projection: { type: "mill", centralMeridian: 0.0 },
-  width: 500
+  width: 1100
 });
