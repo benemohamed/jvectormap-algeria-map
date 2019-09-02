dedbe53d9e59eab92046ab30d8385a80f8a1bfe6
Mohamed Ben <mohamed@ipfinder.io>
2019-09-02 22:52:32 +0100

    Add Communes de la wilaya mostaganem

--- src/Wilaya/cities/en/mostaganem_en.js
+++ src/Wilaya/cities/en/mostaganem_en.js
@@ -1,9 +1,9 @@
 jQuery.fn.vectorMap("addMap", "mostaganem_en", {
   insets: [
     {
-      width: 500,
+      width: 1100,
       top: 0,
-      height: 420,
+      height: 1100,
       bbox: [{ y: 0, x: 300 }, { y: 0, x: 0 }],
       left: 0
     }
@@ -11,10 +11,134 @@ jQuery.fn.vectorMap("addMap", "mostaganem_en", {
   paths: {
     "1": {
       path: "",
-      name: ""
+      name: "Mostaganem"
+    },
+    "2": {
+      path: "",
+      name: "Sayada"
+    },
+    "3": {
+      path: "",
+      name: "Fornaka"
+    },
+    "4": {
+      path: "",
+      name: "Stidia"
+    },
+    "5": {
+      path: "",
+      name: "Ain Nouissy"
+    },
+    "6": {
+      path: "",
+      name: "Hassi Maameche"
+    },
+    "7": {
+      path: "",
+      name: "Ain Tadles"
+    },
+    "8": {
+      path: "",
+      name: "Sour"
+    },
+    "9": {
+      path: "",
+      name: "Oued El Kheir"
+    },
+    "10": {
+      path: "",
+      name: "Sidi Bellater"
+    },
+    "11": {
+      path: "",
+      name: "Kheiredine "
+    },
+    "12": {
+      path: "",
+      name: "Sidi Ali"
+    },
+    "13": {
+      path: "",
+      name: "Abdelmalek Ramdane"
+    },
+    "14": {
+      path: "",
+      name: "Hadjadj"
+    },
+    "15": {
+      path: "",
+      name: "Nekmaria"
+    },
+    "16": {
+      path: "",
+      name: "Sidi Lakhdar"
+    },
+    "17": {
+      path: "",
+      name: "Achaacha"
+    },
+    "18": {
+      path: "",
+      name: "Khadra"
+    },
+    "19": {
+      path: "",
+      name: "Bouguirat"
+    },
+    "20": {
+      path: "",
+      name: "Sirat"
+    },
+    "21": {
+      path: "",
+      name: "Ain Sidi Cherif"
+    },
+    "22": {
+      path: "",
+      name: "Mesra"
+    },
+    "23": {
+      path: "",
+      name: "Mansourah"
+    },
+    "24": {
+      path: "",
+      name: "Souaflia"
+    },
+    "25": {
+      path: "",
+      name: "Ouled Boughalem"
+    },
+    "26": {
+      path: "",
+      name: "Ouled Maallah"
+    },
+    "27": {
+      path: "",
+      name: "Mezghrane"
+    },
+    "28": {
+      path: "",
+      name: "Ain Boudinar"
+    },
+    "29": {
+      path: "",
+      name: "Tazgait"
+    },
+    "30": {
+      path: "",
+      name: "Safsaf"
+    },
+    "31": {
+      path: "",
+      name: "Touahria"
+    },
+    "32": {
+      path: "",
+      name: "El Hassiane"
     }
   },
-  height: 420,
+  height: 1100,
   projection: { type: "mill", centralMeridian: 0.0 },
-  width: 500
+  width: 1100
 });
