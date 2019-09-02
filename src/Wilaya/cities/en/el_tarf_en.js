e1bfa29a3e91e9844ac7fe35b74b60c664ea3441
Mohamed Ben <mohamed@ipfinder.io>
2019-09-02 22:52:33 +0100

    Add Communes de la wilaya el_tarf

--- src/Wilaya/cities/en/el_tarf_en.js
+++ src/Wilaya/cities/en/el_tarf_en.js
@@ -1,9 +1,9 @@
 jQuery.fn.vectorMap("addMap", "el_tarf_en", {
   insets: [
     {
-      width: 500,
+      width: 1100,
       top: 0,
-      height: 420,
+      height: 1100,
       bbox: [{ y: 0, x: 300 }, { y: 0, x: 0 }],
       left: 0
     }
@@ -11,10 +11,102 @@ jQuery.fn.vectorMap("addMap", "el_tarf_en", {
   paths: {
     "1": {
       path: "",
-      name: ""
+      name: "El Tarf"
+    },
+    "2": {
+      path: "",
+      name: "Bouhadjar"
+    },
+    "3": {
+      path: "",
+      name: "Ben Mhidi"
+    },
+    "4": {
+      path: "",
+      name: "Bougous"
+    },
+    "5": {
+      path: "",
+      name: "El Kala"
+    },
+    "6": {
+      path: "",
+      name: "Ain El Assel"
+    },
+    "7": {
+      path: "",
+      name: "El Aioun"
+    },
+    "8": {
+      path: "",
+      name: "Bouteldja"
+    },
+    "9": {
+      path: "",
+      name: "Souarekh"
+    },
+    "10": {
+      path: "",
+      name: "Berrihane"
+    },
+    "11": {
+      path: "",
+      name: "Lac Des Oiseaux"
+    },
+    "12": {
+      path: "",
+      name: "Chefia"
+    },
+    "13": {
+      path: "",
+      name: "Drean"
+    },
+    "14": {
+      path: "",
+      name: "Chihani"
+    },
+    "15": {
+      path: "",
+      name: "Chebaita Mokhtar"
+    },
+    "16": {
+      path: "",
+      name: "Besbes"
+    },
+    "17": {
+      path: "",
+      name: "Asfour"
+    },
+    "18": {
+      path: "",
+      name: "Echatt"
+    },
+    "19": {
+      path: "",
+      name: "Zerizer"
+    },
+    "20": {
+      path: "",
+      name: "Zitouna"
+    },
+    "21": {
+      path: "",
+      name: "Ain Kerma"
+    },
+    "22": {
+      path: "",
+      name: "Oued Zitoun"
+    },
+    "23": {
+      path: "",
+      name: "Hammam Beni Salah"
+    },
+    "24": {
+      path: "",
+      name: "Raml Souk"
     }
   },
-  height: 420,
+  height: 1100,
   projection: { type: "mill", centralMeridian: 0.0 },
-  width: 500
+  width: 1100
 });
