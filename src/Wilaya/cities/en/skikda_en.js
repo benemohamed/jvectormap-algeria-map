bdc1996f15c5d70c050b8bc4ebdbca457e1d3a1f
Mohamed Ben <mohamed@ipfinder.io>
2019-09-02 22:52:32 +0100

    Add Communes de la wilaya skikda

--- src/Wilaya/cities/en/skikda_en.js
+++ src/Wilaya/cities/en/skikda_en.js
@@ -1,9 +1,9 @@
 jQuery.fn.vectorMap("addMap", "skikda_en", {
   insets: [
     {
-      width: 500,
+      width: 1100,
       top: 0,
-      height: 420,
+      height: 1100,
       bbox: [{ y: 0, x: 300 }, { y: 0, x: 0 }],
       left: 0
     }
@@ -11,10 +11,158 @@ jQuery.fn.vectorMap("addMap", "skikda_en", {
   paths: {
     "1": {
       path: "",
-      name: ""
+      name: "Skikda"
+    },
+    "2": {
+      path: "",
+      name: "Ain Zouit"
+    },
+    "3": {
+      path: "",
+      name: "El Hadaik"
+    },
+    "4": {
+      path: "",
+      name: "Azzaba"
+    },
+    "5": {
+      path: "",
+      name: "Djendel Saadi Mohamed"
+    },
+    "6": {
+      path: "",
+      name: "Ain Cherchar"
+    },
+    "7": {
+      path: "",
+      name: "Bekkouche Lakhdar"
+    },
+    "8": {
+      path: "",
+      name: "Benazouz"
+    },
+    "9": {
+      path: "",
+      name: "Es Sebt"
+    },
+    "10": {
+      path: "",
+      name: "Collo"
+    },
+    "11": {
+      path: "",
+      name: "Beni Zid"
+    },
+    "12": {
+      path: "",
+      name: "Kerkera"
+    },
+    "13": {
+      path: "",
+      name: "Ouled Attia"
+    },
+    "14": {
+      path: "",
+      name: "Oued Zehour"
+    },
+    "15": {
+      path: "",
+      name: "Zitouna"
+    },
+    "16": {
+      path: "",
+      name: "El Harrouch"
+    },
+    "17": {
+      path: "",
+      name: "Zerdazas"
+    },
+    "18": {
+      path: "",
+      name: "Ouled Hebaba"
+    },
+    "19": {
+      path: "",
+      name: "Sidi Mezghiche"
+    },
+    "20": {
+      path: "",
+      name: "Emdjez Edchich"
+    },
+    "21": {
+      path: "",
+      name: "Beni Oulbane"
+    },
+    "22": {
+      path: "",
+      name: "Ain Bouziane"
+    },
+    "23": {
+      path: "",
+      name: "Ramdane Djamel"
+    },
+    "24": {
+      path: "",
+      name: "Beni Bachir"
+    },
+    "25": {
+      path: "",
+      name: "Salah Bouchaour"
+    },
+    "26": {
+      path: "",
+      name: "Tamalous"
+    },
+    "27": {
+      path: "",
+      name: "Ain Kechra"
+    },
+    "28": {
+      path: "",
+      name: "Oum Toub"
+    },
+    "29": {
+      path: "",
+      name: "Bein El Ouiden"
+    },
+    "30": {
+      path: "",
+      name: "Fil Fila"
+    },
+    "31": {
+      path: "",
+      name: "Cheraia"
+    },
+    "32": {
+      path: "",
+      name: "Kanoua"
+    },
+    "33": {
+      path: "",
+      name: "El Ghedir"
+    },
+    "34": {
+      path: "",
+      name: "Bouchtata"
+    },
+    "35": {
+      path: "",
+      name: "Ouldja Boulbalout"
+    },
+    "36": {
+      path: "",
+      name: "Kheneg Mayoum"
+    },
+    "37": {
+      path: "",
+      name: "Hamadi Krouma"
+    },
+    "38": {
+      path: "",
+      name: "El Marsa"
     }
   },
-  height: 420,
+  height: 1100,
   projection: { type: "mill", centralMeridian: 0.0 },
-  width: 500
+  width: 1100
 });
