5e0321a5eba9568cb033a6d232a3ea6d79ec597c
Mohamed Ben <mohamed@ipfinder.io>
2019-09-02 22:52:33 +0100

    Add Communes de la wilaya bordj_bou_arreridj

--- src/Wilaya/cities/en/bordj_bou_arreridj_en.js
+++ src/Wilaya/cities/en/bordj_bou_arreridj_en.js
@@ -1,9 +1,9 @@
 jQuery.fn.vectorMap("addMap", "bordj_bou_arreridj_en", {
   insets: [
     {
-      width: 500,
+      width: 1100,
       top: 0,
-      height: 420,
+      height: 1100,
       bbox: [{ y: 0, x: 300 }, { y: 0, x: 0 }],
       left: 0
     }
@@ -11,10 +11,142 @@ jQuery.fn.vectorMap("addMap", "bordj_bou_arreridj_en", {
   paths: {
     "1": {
       path: "",
-      name: ""
+      name: "Bordj Bou Arreridj"
+    },
+    "2": {
+      path: "",
+      name: "Ras El Oued"
+    },
+    "3": {
+      path: "",
+      name: "Bordj Zemoura"
+    },
+    "4": {
+      path: "",
+      name: "Mansoura"
+    },
+    "5": {
+      path: "",
+      name: "El Mhir"
+    },
+    "6": {
+      path: "",
+      name: "Ben Daoud"
+    },
+    "7": {
+      path: "",
+      name: "El Achir"
+    },
+    "8": {
+      path: "",
+      name: "Ain Taghrout"
+    },
+    "9": {
+      path: "",
+      name: "Bordj Ghdir"
+    },
+    "10": {
+      path: "",
+      name: "Sidi Embarek"
+    },
+    "11": {
+      path: "",
+      name: "El Hamadia"
+    },
+    "12": {
+      path: "",
+      name: "Belimour"
+    },
+    "13": {
+      path: "",
+      name: "Medjana"
+    },
+    "14": {
+      path: "",
+      name: "Teniet En Nasr"
+    },
+    "15": {
+      path: "",
+      name: "Djaafra"
+    },
+    "16": {
+      path: "",
+      name: "El Main"
+    },
+    "17": {
+      path: "",
+      name: "Ouled Brahem"
+    },
+    "18": {
+      path: "",
+      name: "Ouled Dahmane"
+    },
+    "19": {
+      path: "",
+      name: "Hasnaoua"
+    },
+    "20": {
+      path: "",
+      name: "Khelil"
+    },
+    "21": {
+      path: "",
+      name: "Taglait"
+    },
+    "22": {
+      path: "",
+      name: "Ksour"
+    },
+    "23": {
+      path: "",
+      name: "Ouled Sidi Brahim"
+    },
+    "24": {
+      path: "",
+      name: "Tafreg"
+    },
+    "25": {
+      path: "",
+      name: "Colla"
+    },
+    "26": {
+      path: "",
+      name: "Tixter"
+    },
+    "27": {
+      path: "",
+      name: "El Ach"
+    },
+    "28": {
+      path: "",
+      name: "El Anseur"
+    },
+    "29": {
+      path: "",
+      name: "Tesmart"
+    },
+    "30": {
+      path: "",
+      name: "Ain Tesra"
+    },
+    "31": {
+      path: "",
+      name: "Bir Kasdali"
+    },
+    "32": {
+      path: "",
+      name: "Ghilassa"
+    },
+    "33": {
+      path: "",
+      name: "Rabta"
+    },
+    "34": {
+      path: "",
+      name: "Haraza"
     }
   },
-  height: 420,
+  height: 1100,
   projection: { type: "mill", centralMeridian: 0.0 },
-  width: 500
+  width: 1100
 });
