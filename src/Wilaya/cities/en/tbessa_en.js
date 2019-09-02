824f6861e05b6a743fb5f032939195d34f067380
Mohamed Ben <mohamed@ipfinder.io>
2019-09-02 22:52:32 +0100

    Add Communes de la wilaya tbessa

--- src/Wilaya/cities/en/tbessa_en.js
+++ src/Wilaya/cities/en/tbessa_en.js
@@ -1,9 +1,9 @@
 jQuery.fn.vectorMap("addMap", "tbessa_en", {
   insets: [
     {
-      width: 500,
+      width: 1100,
       top: 0,
-      height: 420,
+      height: 1100,
       bbox: [{ y: 0, x: 300 }, { y: 0, x: 0 }],
       left: 0
     }
@@ -11,10 +11,118 @@ jQuery.fn.vectorMap("addMap", "tbessa_en", {
   paths: {
     "1": {
       path: "",
-      name: ""
+      name: "Tebessa"
+    },
+    "2": {
+      path: "",
+      name: "Bir El Ater"
+    },
+    "3": {
+      path: "",
+      name: "Cheria"
+    },
+    "4": {
+      path: "",
+      name: "Stah Guentis"
+    },
+    "5": {
+      path: "",
+      name: "El Aouinet"
+    },
+    "6": {
+      path: "",
+      name: "Lahouidjbet"
+    },
+    "7": {
+      path: "",
+      name: "Safsaf El Ouesra"
+    },
+    "8": {
+      path: "",
+      name: "Hammamet"
+    },
+    "9": {
+      path: "",
+      name: "Negrine"
+    },
+    "10": {
+      path: "",
+      name: "Bir El Mokadem"
+    },
+    "11": {
+      path: "",
+      name: "El Kouif"
+    },
+    "12": {
+      path: "",
+      name: "Morsott"
+    },
+    "13": {
+      path: "",
+      name: "El Ogla"
+    },
+    "14": {
+      path: "",
+      name: "Bir Dheheb"
+    },
+    "15": {
+      path: "",
+      name: "El Ogla El Malha"
+    },
+    "16": {
+      path: "",
+      name: "Gorriguer"
+    },
+    "17": {
+      path: "",
+      name: "Bekkaria"
+    },
+    "18": {
+      path: "",
+      name: "Boukhadra"
+    },
+    "19": {
+      path: "",
+      name: "Ouenza"
+    },
+    "20": {
+      path: "",
+      name: "El Ma El Biodh"
+    },
+    "21": {
+      path: "",
+      name: "Oum Ali"
+    },
+    "22": {
+      path: "",
+      name: "Thlidjene"
+    },
+    "23": {
+      path: "",
+      name: "Ain Zerga"
+    },
+    "24": {
+      path: "",
+      name: "El Meridj"
+    },
+    "25": {
+      path: "",
+      name: "Boulhaf Dyr"
+    },
+    "26": {
+      path: "",
+      name: "Bedjene"
+    },
+    "27": {
+      path: "",
+      name: "El Mazeraa"
+    },
+    "28": {
+      path: "",
+      name: "Ferkane"
     }
   },
-  height: 420,
+  height: 1100,
   projection: { type: "mill", centralMeridian: 0.0 },
-  width: 500
+  width: 1100
 });
