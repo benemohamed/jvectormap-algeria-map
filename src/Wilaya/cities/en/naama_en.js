b2d0111bb4242bdfec229713c159be9ca731d25f
Mohamed Ben <mohamed@ipfinder.io>
2019-09-02 22:52:33 +0100

    Add Communes de la wilaya naama

--- src/Wilaya/cities/en/naama_en.js
+++ src/Wilaya/cities/en/naama_en.js
@@ -1,9 +1,9 @@
 jQuery.fn.vectorMap("addMap", "naama_en", {
   insets: [
     {
-      width: 500,
+      width: 1100,
       top: 0,
-      height: 420,
+      height: 1100,
       bbox: [{ y: 0, x: 300 }, { y: 0, x: 0 }],
       left: 0
     }
@@ -11,10 +11,54 @@ jQuery.fn.vectorMap("addMap", "naama_en", {
   paths: {
     "1": {
       path: "",
-      name: ""
+      name: "Naama"
+    },
+    "2": {
+      path: "",
+      name: "Mechria"
+    },
+    "3": {
+      path: "",
+      name: "Ain Sefra"
+    },
+    "4": {
+      path: "",
+      name: "Tiout"
+    },
+    "5": {
+      path: "",
+      name: "Sfissifa"
+    },
+    "6": {
+      path: "",
+      name: "Moghrar"
+    },
+    "7": {
+      path: "",
+      name: "Assela"
+    },
+    "8": {
+      path: "",
+      name: "Djeniane Bourzeg"
+    },
+    "9": {
+      path: "",
+      name: "Ain Ben Khelil"
+    },
+    "10": {
+      path: "",
+      name: "Makman Ben Amer"
+    },
+    "11": {
+      path: "",
+      name: "Kasdir"
+    },
+    "12": {
+      path: "",
+      name: "El Biod"
     }
   },
-  height: 420,
+  height: 1100,
   projection: { type: "mill", centralMeridian: 0.0 },
-  width: 500
+  width: 1100
 });
